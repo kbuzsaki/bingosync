634d08f0d5bc9cea30300a33ca6b0378fa4223e0
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2015-06-06 09:34:37 -0700

    Added database configs

--- bingosync/settings.py
+++ bingosync/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'bingosync.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bingosync',
+        'USER': 'bingoserv',
+        'PASSWORD': 'password',
     }
 }
 
