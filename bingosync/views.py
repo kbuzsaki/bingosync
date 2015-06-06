1e09b1b2ec1b68bf2c4007d1e1b6a6b6d37b50ce
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2015-06-06 12:08:55 -0700

    Added stub for html view of board

--- bingosync/views.py
+++ bingosync/views.py
@@ -6,6 +6,9 @@ from .bingo_generator import BingoGenerator
 def index(request):
     return HttpResponse("Hello, world.")
 
+def board_view(request, seed):
+    return HttpResponse("You requested a board for seed: " + str(seed))
+
 def board_json(request, seed):
     generator = BingoGenerator.instance()
     card = generator.get_card(seed)
