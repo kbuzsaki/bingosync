ebd919697b299278d78f437ea4bdaa5421118e33
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2015-06-06 09:49:11 -0700

    Added first view

--- bingosync/urls.py
+++ bingosync/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 ]
+
