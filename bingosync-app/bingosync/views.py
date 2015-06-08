609acb5bc2219cd40545d75e2050ba95012975fc
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2015-06-08 13:18:00 -0700

    Added ajax route for client put instead of websocket

--- bingosync-app/bingosync/views.py
+++ bingosync-app/bingosync/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 from .bingo_generator import BingoGenerator
 
@@ -14,4 +16,9 @@ def board_json(request, seed):
     generator = BingoGenerator.instance()
     card = generator.get_card(seed)
     return JsonResponse(card, safe=False)
+
+@csrf_exempt
+def goal_selected(request):
+    data = json.loads(request.body.decode("utf8"))
+    return HttpResponse("Got goal: " + data["goal"])
 
