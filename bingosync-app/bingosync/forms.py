aebe8ccfbb8921366e3b0217bb53839ac3156524
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2015-06-27 12:48:28 -0700

    Switched from hardcoded lengths in form fields to max_length of model field

--- bingosync-app/bingosync/forms.py
+++ bingosync-app/bingosync/forms.py
@@ -1,13 +1,17 @@
 from django import forms
 from django.db import transaction
 
-from .models import Room, Game
+from .models import Room, Game, Player
 from .bingo_generator import BingoGenerator
 
+
+ROOM_NAME_MAX_LENGTH = Room._meta.get_field("name").max_length
+PLAYER_NAME_MAX_LENGTH = Player._meta.get_field("name").max_length
+
 class RoomForm(forms.Form):
-    room_name = forms.CharField(label="Room Name", max_length=255)
+    room_name = forms.CharField(label="Room Name", max_length=ROOM_NAME_MAX_LENGTH)
     password = forms.CharField(label="Passphrase", widget=forms.PasswordInput())
-    nickname = forms.CharField(label="Nickname", max_length=50)
+    nickname = forms.CharField(label="Nickname", max_length=PLAYER_NAME_MAX_LENGTH)
     seed = forms.CharField(label="Seed", widget=forms.NumberInput())
 
     def create_room(self):
