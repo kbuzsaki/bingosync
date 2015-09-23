48ec576f3d0401e834472d40e226eb7545c796f8
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2015-09-23 00:16:21 -0700

    Added localhost to ALLOWED_HOSTS

--- bingosync-app/bingosync/settings.py
+++ bingosync-app/bingosync/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = secret_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = not IS_PROD
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
