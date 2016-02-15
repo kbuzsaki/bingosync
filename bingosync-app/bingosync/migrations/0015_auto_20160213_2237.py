43afbf875f79581579f479ef8d7b6132321a8f8a
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2016-02-14 20:48:13 -0800

    Fixed migration incorrectly importing Square model

--- bingosync-app/bingosync/migrations/0015_auto_20160213_2237.py
+++ bingosync-app/bingosync/migrations/0015_auto_20160213_2237.py
@@ -5,7 +5,7 @@ from django.db import models, migrations
 from bingosync.models import CompositeColor, Color
 
 def update_old_square_colors(apps, schema_editor):
-    Square = apps.get_model('bingosync', Square)
+    Square = apps.get_model('bingosync', 'Square')
     for square in Square.objects.all():
         composite_color = CompositeColor([Color.for_value(square.color_value)])
         square.color_value = composite_color.value
