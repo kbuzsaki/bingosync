a1870e7a4f26570948860d96addb1dc9eb0c4c69
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2016-06-09 12:02:26 -0700

    Added full game title on hover when abbreviation is used

--- bingosync-app/bingosync/templatetags/app_filters.py
+++ bingosync-app/bingosync/templatetags/app_filters.py
@@ -1,7 +1,15 @@
 from django import template
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 @register.filter
 def num_goals(game, color):
     return len([square for square in game.squares if square.color == color])
+
+@register.filter
+def hovertext_game_type(game_type):
+    title = escape(game_type.long_name)
+    short_name = escape(str(game_type))
+    return mark_safe('<span title="' + title + '">' + short_name + '</span>')
