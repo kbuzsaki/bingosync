6151dca7490d9c76f82c9735b32dc385d1a3ccb8
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2016-08-20 00:52:00 -0700

    Update pokemon snap generator

--- bingosync-app/generators/pokemon_snap_generator.js
+++ bingosync-app/generators/pokemon_snap_generator.js
@@ -215,57 +215,54 @@ bingoGenerator = function(bingoList, opts) {
         }
         return bingoBoard;
     }
-}
+};
 var bingoList = [];
 bingoList[1] = [
-    { name: "Pic: Gust-using Pidgey", types: ["beach"] },
-    { name: "Pic: Sleeping Snorlax", types: ["beach"] },
-    { name: "Pic: Vulpix (Uncentered)", types: ["colcano"] },
+    { name: "Pic: Pidgey using Gust", types: ["beach"] },
+    { name: "Add a pic of sleeping Snorlax to the album", types: ["beach"] },
+    { name: "Pic: Vulpix (Uncentered)", types: ["volcano"] },
     { name: "Look at Butterfree in report", types: ["report"] },
-    { name: "Drive out of Tunnel", types: ["tunnel","drive"] },
-    { name: "Unlock Pester Balls", types: ["perster"] }
+    { name: "Drive out of Tunnel", types: ["tunnel","drive"] }
 ];
 bingoList[2] = [
-    { name: "Make Pikachu ride Electrode", types: ["tunnel"] },
-    { name: "Make three Dugtrios appear", types: ["tunnel"] },
+    { name: "Pic: Pikachu riding Electrode", types: ["tunnel"] },
+    { name: "Pic: Three Dugtrios", types: ["tunnel"] },
     { name: "Pic: Fainted Doduo", types: ["beach"] },
-    { name: "Throw Meowth down the hill", types: ["beach","perster"] },
-    { name: "Pic: Slowbro", types: ["river"] },
+    { name: "Throw Meowth down the hill", types: ["beach","pester"] },
+    { name: "Pic: Speed Pikachu", types: ["river"] },
     { name: "Pic: Charmeleon", types: ["volcano"] }
 ];
 bingoList[3] = [
-    { name: "Release three Porygons", types: ["river","perster"] },
-    { name: "Pic: Speed Pikachu", types: ["river"] },
-    { name: "Pic: Shellder Sign", types: ["river"] },
+    { name: "Release three Porygons", types: ["river","pester"] },
+    { name: "Add a pic of the Shellder board to your album", types: ["river"] },
     { name: "Pic: Surfer Pikachu", types: ["beach"] },
-    { name: "Add a Rapidash to your album", types: ["volcano","album"] },
-    { name: "Pic: Wonderful Zubat (Cave)", types: ["cave"] },
+    { name: "Add a pic of Rapidash to your album", types: ["volcano","album"] },
+    { name: "Pic: Centered Zubat (Cave)", types: ["cave"] },
     { name: "Pic: Beach Sign", types: ["beach"] }
 ];
 bingoList[4] = [
     { name: "Hit a Kakuna with an apple", types: ["river"] },
     { name: "Make Victreebel appear", types: ["cave"] },
     { name: "Full camera film in Tunnel", types: ["film"] },
-    { name: "Throw a perster ball at Electrabuzz", types: ["perster","tunnel"] },
-    { name: "3000+ Pic of Butterfree", types: ["beach"] },
+    { name: "Throw a pester ball at Electabuzz", types: ["pester","tunnel"] },
+    { name: "Pic: 3000+ Butterfree", types: ["beach"] },
     { name: "Pic: Digging Sandslash", types: ["valley"] }
 ];
 bingoList[5] = [
-    { name: "1000- Pic of Electrode", types: ["tunnel"] },
+    { name: "Pic: 1000- Electrode", types: ["tunnel"] },
     { name: "Save Meowth from the Pidgeys", types: ["beach"] },
-    { name: "Unlock the PokeFlute", types: ["flute"] },
     { name: "Make a sparkling Dratini spawn", types: ["valley"] },
     { name: "Pic: Climbing Graveler", types: ["valley","graveler"] },
-    { name: "Wonderful pic of Jynx", types: ["cave"] },
-    { name: "make both Electrabuzz activate the TV screens", types: ["tunnel"] }
+    { name: "Pic: Centered Jynx", types: ["cave"] },
+    { name: "Make both Electabuzz activate the TV screens", types: ["tunnel"] }
 ];
 bingoList[6] = [
     { name: "Pic: Running Poliwag", types: ["river"] },
-    { name: "Get all Poliwags into the water", types: ["river","perster"] },
-    { name: "Pic: Sparkling Magikarp (Cave)", types: ["cave","perster"] },
-    { name: "4000+ Haunter", types: ["tunnel"] },
-    { name: "Volcano Sign", types: ["sign","volcano"] },
-    { name: "Pester ball a Vulpix", types: ["volcano","perster"] },
+    { name: "Get all Poliwags into the water", types: ["river","pester"] },
+    { name: "Pic: Sparkling Magikarp (Cave)", types: ["cave","pester"] },
+    { name: "Pic: 4000+ Haunter", types: ["tunnel"] },
+    { name: "Pic: Volcano Sign", types: ["sign","volcano"] },
+    { name: "Throw a Pester ball at Vulpix", types: ["volcano","pester"] },
     { name: "Crash into the egg of Moltres", types: ["volcano"] }
 ];
 bingoList[7] = [
@@ -273,147 +270,149 @@ bingoList[7] = [
     { name: "Add a Pic of a palm tree to the album", types: ["album"] },
     { name: "Pic: Magikarp in Volcano", types: ["castle"] },
     { name: "Pic: Geodude w/ bonus points", types: ["valley"] },
-    { name: "3000+ Shellder", types: ["river"] },
-    { name: "300- Jynx", types: ["cave"] }
+    { name: "Pic: 3000+ Shellder", types: ["river"] },
+    { name: "Pic: 300- Jynx", types: ["cave"] },
+    { name: "Pic: Magikarp in six different stages", types: ["valley"] }
 ];
 bingoList[8] = [
     { name: "Pic: Two Muks", types: ["cave"] },
     { name: "Make both Slowbros spawn", types: ["river"] },
     { name: "Drive out of Cave", types: ["cave","drive"] },
-    { name: "Wonderful pic of Arcanine", types: ["volcano"] },
-    { name: "Wonderful pic of Magikarp in River", types: ["river"] },
-    { name: "3000+ Doduo", types: ["beach"] },
-    { name: "4000+ Slowbro", types: ["river"] },
-    { name: "Unlock Valley", types: ["valley","rainbow","flute"] }
+    { name: "Pic: Centered Arcanine", types: ["volcano"] },
+    { name: "Pic: Centered Magikarp in River", types: ["river"] },
+    { name: "Pic: 3000+ Doduo", types: ["beach"] },
+    { name: "Pic: 4000+ Slowbro", types: ["river"] }
 ];
 bingoList[9] = [
     { name: "Pic: Jigglypuff Trio", types: ["cave"] },
     { name: "Pic: Fainted Weepinbell", types: ["cave"] },
-    { name: "Wonderful pic of Staryu", types: ["valley"] },
+    { name: "Pic: Centered Staryu", types: ["valley"] },
     { name: "Pic: Magikarp lying on the floor", types: ["valley"] },
     { name: "Hit the last Electrode in Tunnel without activating the cutscene", types: ["tunnel"] },
-    { name: "Add a pic of a dead tree to the album", types: ["valley"] }
+    { name: "Add a pic of a dead tree to the album", types: ["valley"] },
+    { name: "Pic: Interrupt the performance", types: ["cave"] }
 ];
 bingoList[10] = [
     { name: "Pic: Mid-air Sandshrew", types: ["valley"] },
     { name: "Pic: Dancing Slowpoke", types: ["river","flute"] },
     { name: "Pic: Fighting Magmars", types: ["volcano"] },
     { name: "Pic: Balloon Pikachu", types: ["cave"] },
-    { name: "4000+ Porygon", types: ["river"] },
-    { name: "Lave Volcano w/ 4 pics left", types: ["volcano"] },
-    { name: "3200+ Psyduck", types: ["river"] }
+    { name: "Pic: 4000+ Porygon", types: ["river"] },
+    { name: "Leave Volcano w/ 4 pics left", types: ["volcano"] },
+    { name: "Pic: 3200+ Psyduck", types: ["river"] }
 ];
 bingoList[11] = [
     { name: "Pic: Two Cloysters", types: ["river"] },
     { name: "Make Charizard spawn", types: ["volcano"] },
     { name: "Pic: Three Vulpix", types: ["volcano"] },
     { name: "Hit a Cloyster with an apple", types: ["river"] },
-    { name: "3500+ Charizard", types: ["volcano"] },
+    { name: "Pic: 3500+ Charizard", types: ["volcano"] },
     { name: "Throw twelve apples into a trunk", types: ["river"] },
-    { name: "2500+ Weepinbell", types: ["cave"] },
-    { name: "Pics of all three Pikachus in Tunnel in the album", types: ["tunnel","album"] }
+    { name: "Pic: 2500+ Weepinbell", types: ["cave"] },
+    { name: "Add pics of all three Pikachus in Tunnel to the album", types: ["tunnel","album"] }
 ];
 bingoList[12] = [
-    { name: "Dancing Meowth pic", types: ["beach","flute"] },
+    { name: "Pic: Dancing Meowth", types: ["beach","flute"] },
     { name: "Make Magmar attack Charmander", types: ["volcano"] },
-    { name: "500- Moltres", types: ["volcano"] },
+    { name: "Pic: 500- Moltres", types: ["volcano"] },
     { name: "Pic: Kangaskhan in the air", types: ["beach"] },
-    { name: "Pic: Snorlax with both feet in the air", types: ["beach"] }
+    { name: "Pic: Snorlax with both feet in the air", types: ["beach"] },
+    { name: "Evolve six different pokemon", types: ["cave"] }
 ];
 bingoList[13] = [
-    { name: "Wonderful pic of four Metapods", types: ["river"] },
-    { name: "3000+ Chansey", types: ["beach"] },
-    { name: "Wonderful pic of the Porygon activating the switch", types: ["river"] },
-    { name: "Make all four Bulbasaurs in Cave evolve", types: ["perster","bulbasaur"] },
-    { name: "3000+ Ditto", types: ["cave"] },
+    { name: "Pic: Centered pic of four Metapods", types: ["river"] },
+    { name: "Pic: 3000+ Chansey", types: ["beach"] },
+    { name: "Pic: Centered pic of the Porygon activating the switch", types: ["river"] },
+    { name: "Make all four Bulbasaurs in Cave evolve", types: ["pester","bulbasaur"] },
+    { name: "Pic: 3000+ Ditto", types: ["cave"] },
     { name: "Hit Moltres with an apple", types: ["volcano"] },
-    { name: "Wonderful pic of Magikarp in Beach", types: ["beach"] }
+    { name: "Pic: Centered of Magikarp in Beach", types: ["beach"] }
 ];
 bingoList[14] = [
-    { name: "3000+ Scyther", types: ["beach","perster"] },
-    { name: "3500+ Kangaskhan", types: ["beach"] },
-    { name: "3000+ Cloyster", types: ["river"] },
-    { name: "3000+ Koffing", types: ["cave"] },
-    { name: "3000+ Zubat", types: ["cave"] },
-    { name: "4000+ Rapidash", types: ["volcano"] },
-    { name: "3000+ Charmeleon", types: ["volcano"] },
-    { name: "2000+ Dratini", types: ["valley"] },
-    { name: "4000+ Gyarados", types: ["valley"] }
+    { name: "Pic: 3000+ Scyther", types: ["beach","pester"] },
+    { name: "Pic: 3500+ Kangaskhan", types: ["beach"] },
+    { name: "Pic: 3000+ Cloyster", types: ["river"] },
+    { name: "Pic: 3000+ Koffing", types: ["cave"] },
+    { name: "Pic: 3000+ Zubat", types: ["cave"] },
+    { name: "Pic: 4000+ Rapidash", types: ["volcano"] },
+    { name: "Pic: 3000+ Charmeleon", types: ["volcano"] },
+    { name: "Pic: 2000+ Dratini", types: ["valley"] },
+    { name: "Pic: 4000+ Gyarados", types: ["valley"] }
 ];
 bingoList[15] = [
-    { name: "4500+ Dragonite", types: ["valley","perster"] },
-    { name: "Pic of Rapidash standing on the front legs", types: ["volcano"] },
-    { name: "Make Sandslash faint with a perster ball", types: ["valley","perster"] },
-    { name: "Get eight different Pokemon in Beach", types: ["beach"] },
+    { name: "Pic: 4000+ Dragonite", types: ["valley","pester"] },
+    { name: "Pic: Rapidash standing on front legs", types: ["volcano"] },
+    { name: "Make Sandslash faint with a pester ball", types: ["valley","pester"] },
+    { name: "Submit eight different Pokemon pics in Beach", types: ["beach"] },
     { name: "Crash into a Dratini with your cart", types: ["valley"] },
     { name: "Pic: Four different Pokemon", types: ["rainbow"] }
 ];
 bingoList[16] = [
     { name: "Pic: Jumping Graveler", types: ["valley","graveler"] },
-    { name: "20 pics of the first Grimer in Cave", types: ["cave"] },
-    { name: "15+ pics in the album", types: ["album"] },
-    { name: "Sparkling Goldeen pic", types: ["valley","perster"] },
+    { name: "Take 20 pics of the first Grimer in Cave", types: ["cave"] },
+    { name: "Add 15+ pics in the album", types: ["album"] },
+    { name: "Pic: Sparkling Goldeen", types: ["valley","pester"] },
     { name: "Pic: Six Charmanders dancing", types: ["volcano","flute"] },
-    { name: "Dancing Gravelers pic", types: ["valley","flute","graveler"] },
+    { name: "Pic: Dancing Gravelers", types: ["valley","flute","graveler"] },
     { name: "Crash into a Vulpix", types: ["volcano"] },
-    { name: "4000+ Kakuna", types: ["cave"] }
+    { name: "Pic: 4000+ Kakuna", types: ["cave"] }
 ];
 bingoList[17] = [
     { name: "Full camera film in Rainbow Cloud", types: ["film","rainbow"] },
-    { name: "Submit an uncentered Moltres pic", types: ["volcano"] },
+    { name: "Pic: Uncentered Moltres", types: ["volcano"] },
     { name: "Pic: Zapdos activating the generator", types: ["tunnel"] },
-    { name: "Cave sign", types: ["cave","sign"] },
-    { name: "River sign", types: ["river","sign"] },
-    { name: "Tunnel sign", types: ["sign","tunnel"] }
+    { name: "Pic: Cave sign", types: ["cave","sign"] },
+    { name: "Pic: River sign", types: ["river","sign"] },
+    { name: "Pic: Tunnel sign", types: ["sign","tunnel"] }
 ];
 bingoList[18] = [
-    { name: "Pics of three different Snorlax dances in the album", types: ["flute","beach","album"] },
-    { name: "Four pics of four different Mankeys in the album", types: ["valley","album"] },
-    { name: "Knock out three different Mankeys", types: ["valley","perster"] },
-    { name: "4200+ Gyarados", types: ["perster","valley"] },
-    { name: "Wonderful pic of Starmie", types: ["valley"] },
-    { name: "Pic: Three Squirtles", types: ["valley","perster"] }
+    { name: "Add pics of three different Snorlax dances to the album", types: ["flute","beach","album"] },
+    { name: "Add pics of four different Mankeys to the album", types: ["valley","album"] },
+    { name: "Knock out three different Mankeys", types: ["valley","pester"] },
+    { name: "Pic: 4200+ Gyarados", types: ["pester","valley"] },
+    { name: "Pic: Centered Starmie", types: ["valley"] },
+    { name: "Pic: Three Squirtles", types: ["valley","pester"] }
 ];
 bingoList[19] = [
     { name: "Pic: Haunter and a flying apple", types: ["tunnel"] },
     { name: "Make the second Scyther spawn", types: ["beach"] },
-    { name: "Pics of all three Vileplume dances in the album", types: ["river","flute","album"] },
+    { name: "Add pics of three different Vileplume dances to the album", types: ["river","flute","album"] },
     { name: "Pic: Three Growlithes", types: ["volcano"] },
-    { name: "3500+ Magnemite", types: ["tunnel"] },
-    { name: "3500+ Magneton", types: ["tunnel"] },
-    { name: "3750+ Snorlax", types: ["beach"] },
-    { name: "3000+ Zapdos", types: ["tunnel"] },
-    { name: "3000+ Lapras", types: ["beach"] }
+    { name: "Pic: 3300+ Magnemite", types: ["tunnel"] },
+    { name: "Pic: 3500+ Magneton", types: ["tunnel"] },
+    { name: "Pic: 3750+ Snorlax", types: ["beach"] },
+    { name: "Pic: 3000+ Zapdos", types: ["tunnel"] },
+    { name: "Pic: 3000+ Lapras", types: ["beach"] }
 ];
 bingoList[20] = [
-    { name: "Six pics of Charizard spitting fire", types: ["volcano"] },
-    { name: "Pics of five different exploded Electrodes in the album", types: ["apple","tunnel","album"] },
-    { name: "Pics of all three legendary bird eggs in the album", types: ["album"] },
-    { name: "Pic of two combined Magnemites in the album", types: ["tunnel"] },
-    { name: "Unlock Rainbow Cloud", types: ["rainbow"] },
-    { name: "3500+ Electrode", types: ["tunnel"] },
-    { name: "5000+ Pidgey", types: ["beach"] },
-    { name: "4800+ Magmar", types: ["volcano"] }
+    { name: "Take six pics of Charizard spitting fire", types: ["volcano"] },
+    { name: "Add pics of five different exploded Electrodes to the album", types: ["apple","tunnel","album"] },
+    { name: "Add pics of all three legendary bird eggs to the album", types: ["album"] },
+    { name: "Add pic of two combined Magnemites to the album", types: ["tunnel"] },
+    { name: "Pic: 3500+ Electrode", types: ["tunnel"] },
+    { name: "Pic: 5000+ Pidgey", types: ["beach"] },
+    { name: "Pic: 4800+ Magmar", types: ["volcano"] }
 ];
 bingoList[21] = [
-    { name: "pic of a jumping Vileplume in the album", types: ["river","flute","album"] },
-    { name: "4400+ Jigglypuff", types: ["cave"] },
-    { name: "4000+ Grimer", types: ["cave"] },
-    { name: "4000+ Victreebel", types: ["cave"] },
-    { name: "3500+ Poliwag", types: ["river"] }
+    { name: "Add pic of a jumping Vileplume to the album", types: ["river","flute","album"] },
+    { name: "Pic: 4400+ Jigglypuff", types: ["cave"] },
+    { name: "Pic: 4000+ Grimer", types: ["cave"] },
+    { name: "Pic: 4000+ Victreebel", types: ["cave"] },
+    { name: "Pic: 3500+ Poliwag", types: ["river"] },
+    { name: "Knock down all five Geodudes", types: ["valley"] }
 ];
 bingoList[22] = [
-    { name: "4500+ Moltres", types: ["volcano"] },
+    { name: "Pic: 4500+ Moltres", types: ["volcano"] },
     { name: "Pic: Two Psyducks", types: ["river"] },
     { name: "Snipe first Psyduck w/o using boost (River)", types: ["river"] },
-    { name: "Wonderful pic of Metapod climbing back up", types: ["river"] },
+    { name: "Pic: Centered Metapod climbing back up", types: ["river"] },
     { name: "Crash into a jumping Psyduck", types: ["river"] },
-    { name: "4000+ Muk", types: ["cave"] },
-    { name: "500- Koffing", types: ["cave"] }
+    { name: "Pic: 3900+ Muk", types: ["cave"] },
+    { name: "Pic: 500- Koffing", types: ["cave"] }
 ];
 bingoList[23] = [
-    { name: "4800 Sandslash", types: ["valley"] },
-    { name: "Pics of two different Charmeleons lying on the floor", types: ["volcano","perster"] },
+    { name: "Pic: 4800 Sandslash", types: ["valley"] },
+    { name: "Add pics of two different Charmeleons on the floor to the album", types: ["volcano","pester"] },
     { name: "Pic: Exactly three Charmanders dancing", types: ["volcano"] },
     { name: "Pic: Three Rapidashes", types: ["volcano"] },
     { name: "Pic: One wing of Zubat only", types: ["tunnel","cave"] },
@@ -422,19 +421,18 @@ bingoList[23] = [
     { name: "Look at Mew in the Pokemon Report", types: ["rainbow"] }
 ];
 bingoList[24] = [
-    { name: "Pic: Three Arcanines", types: ["volcano","perster"] },
+    { name: "Pic: Two Arcanines", types: ["volcano","pester"] },
     { name: "Pic: Two Electabuzz eating the same apple", types: ["tunnel"] },
-    { name: "Watch the end credits", types: ["rainbow"] },
-    { name: "3000- Mew", types: ["rainbow"] },
-    { name: "3400+ Diglett", types: ["tunnel"] },
-    { name: "Pester Ball a Zubat in Cave", types: ["cave","perster"] }
+    { name: "Pic: 3000- Mew", types: ["rainbow"] },
+    { name: "Pic: 3200+ Diglett", types: ["tunnel"] },
+    { name: "Pester Ball a Zubat in Cave", types: ["cave","pester"] },
+    { name: "Pic: 9000+ Mew", types: ["rainbow"] }
 ];
 bingoList[25] = [
     { name: "Pic: Mew facing you", types: ["rainbow"] },
-    { name: "11 Wonderful pics of different Pokemon in Valley", types: ["valley"] },
+    { name: "Submit 11 Centered pics of different Pokemon in Valley", types: ["valley"] },
     { name: "Pic: Pikachu riding Articuno", types: ["cave"] },
-    { name: "Two Pokemon signs and their corresponding Pokemon in the album", types: ["volcano"] },
+    { name: "Add two Pokemon signs and their corresponding Pokemon to the album", types: ["volcano"] },
     { name: "Throw 200 apples into the river in Valley", types: ["valley","apple"] },
-    { name: "Challenge score in Beach", types: ["challenge"] },
-    { name: "Challenge score in Volcano", types: ["challenge"] }
+    { name: "Challenge score in Beach (1,510,000)", types: ["challenge"] }
 ];
