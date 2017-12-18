609d2dca82d3278ce510c3e1d56d5e2bbd4049f3
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2017-12-17 16:27:20 -0800

    Make test_generator.py have a test per game, seed combo using metaprogramming magic

--- bingosync-app/bingosync/test_generator.py
+++ bingosync-app/bingosync/test_generator.py
@@ -1,5 +1,6 @@
 from django import test
 
+import itertools
 import json
 import os
 import sys
@@ -7,6 +8,7 @@ import sys
 from .models import GameType
 from .settings import GEN_TESTDATA_DIR
 
+TEST_TYPES = [game_type for game_type in GameType if game_type != GameType.custom]
 TEST_SEEDS = [1, 1000, 1234, 12345]
 
 def get_golden_data(game_type, seed):
@@ -17,22 +19,41 @@ def get_golden_data(game_type, seed):
     with open(output_path) as infile:
         return json.load(infile)
 
+def test_get_card(self, game_type):
+    """ Tests whether the generator generates any valid card """
+    board_json = game_type.generator_instance().get_card(1)
+    self.assertEqual(len(board_json), 25)
+
+def test_card_correctness(self, game_type, seed):
+    """ Tests whether the generator generates the correct card as compared to golden data """
+    board_json = game_type.generator_instance().get_card(seed)
+    golden_json = get_golden_data(game_type, seed)
+    msg = "game: " + game_type.name + ", seed: " + str(seed)
+    self.assertListEqual(board_json, golden_json, msg=msg)
+
+def make_test(testfn, args):
+    """ Generate a test method invoking testfn but passing args along to it.
+        For use with parametrize_test() below.
+    """
+    def fn(self):
+        testfn(self, *args)
+    return fn
+
+def parametrize_test(test_class, name_template, testfn, arglists):
+    """ Dynamically add testcases to the given test class by taking the product of the
+        argument lists. For example:
+            parametrize_test(SomeTest, "something_{}_{}", some_fn, [colors, animals])
+        will add a test method to SomeTest for every (color, animal) pair that invokes
+        some_fn(color, animal). It also names the function using the template, e.g.
+        "test_something_{color}_{animal}".
+    """
+    for args in itertools.product(*arglists):
+        test_name = "test_" + name_template.format(*args)
+        test_func = make_test(testfn, args)
+        setattr(test_class, test_name, test_func)
+
 class GeneratorTest(test.SimpleTestCase):
-
-    def setUp(self):
-        self.testable_types = [game_type for game_type in GameType if game_type != GameType.custom]
-
-    def test_get_card(self):
-        """ Tests whether the generator generates any valid card """
-        for game_type in self.testable_types:
-            board_json = game_type.generator_instance().get_card(1)
-            self.assertEqual(len(board_json), 25)
-
-    def test_card_correctness(self):
-        """ Tests whether the generator generates the correct card as compared to golden data """
-        for game_type in self.testable_types:
-            for seed in TEST_SEEDS:
-                board_json = game_type.generator_instance().get_card(seed)
-                golden_json = get_golden_data(game_type, seed)
-                msg = "game: " + game_type.name + ", seed: " + str(seed)
-                self.assertListEqual(board_json, golden_json, msg=msg)
+    """ Shell test class that has test methods added later with parametrize_test() below. """
+    pass
+parametrize_test(GeneratorTest, "get_card_{.name}", test_get_card, [TEST_TYPES])
+parametrize_test(GeneratorTest, "correctness_{.name}_{}", test_card_correctness, [TEST_TYPES, TEST_SEEDS])
