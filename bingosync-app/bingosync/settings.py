9b81a3db72d095b11748d153b60cc430de25b5bc
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2017-12-27 09:04:33 -0800

    Remove django suit in preparation for redesigned admin ui in django 1.9

--- bingosync-app/bingosync/settings.py
+++ bingosync-app/bingosync/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 # Application definition
 
 INSTALLED_APPS = (
-    'suit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
