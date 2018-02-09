c2338b9bac4aa06079e2a8cf49271c648773dae4
Charles Burnham <felixtheslayer@gmail.com>
2018-02-09 00:01:55 -0600

    Fix room settings panel collapse, format js values properly

--- bingosync-app/bingosync/templatetags/app_filters.py
+++ bingosync-app/bingosync/templatetags/app_filters.py
@@ -1,6 +1,7 @@
 from django import template
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
+from numbers import Number
 
 register = template.Library()
 
@@ -13,3 +14,15 @@ def hovertext_game_type(game_type):
     title = escape(game_type.long_name)
     short_name = escape(str(game_type))
     return mark_safe('<span title="' + title + '">' + short_name + '</span>')
+
+@register.filter
+def format_js_val(value):
+    if value is True:
+        return "true"
+    elif value is False:
+        return "false"
+    elif isinstance(value, Number):
+        return value
+    else:
+        return '"' + value + '"'
+
