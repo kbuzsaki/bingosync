9ef2e008e029d9a5db8bd9f74bc48d87323361cb
Kyle Buzsaki <kbuzsaki@gmail.com>
2018-03-17 17:23:04 -0700

    Add preventDefault() to idle room buttons

--- bingosync-app/static/bingosync/home.js
+++ bingosync-app/static/bingosync/home.js
@@ -1,9 +1,11 @@
 
 function initializeRoomsPanel($roomsPanel) {
-    $roomsPanel.find(".show-button").on("click", function() {
+    $roomsPanel.find(".show-button").on("click", function(e) {
+        e.preventDefault();
         $roomsPanel.removeClass("hide-idle-rooms");
     });
-    $roomsPanel.find(".hide-button").on("click", function() {
+    $roomsPanel.find(".hide-button").on("click", function(e) {
+        e.preventDefault();
         $roomsPanel.addClass("hide-idle-rooms");
     });
 }
