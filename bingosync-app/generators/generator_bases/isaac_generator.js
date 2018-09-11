39df3c5d205e91a99fa3a83409f84b6a17b4493c
Kyle Buzsaki <kbuzsaki@gmail.com>
2018-09-11 07:46:19 -0700

    Update Binding of Isaac generator

--- bingosync-app/generators/generator_bases/isaac_generator.js
+++ bingosync-app/generators/generator_bases/isaac_generator.js
@@ -1,4 +1,4 @@
- (function(j, i, g, m, k, n, o) { function q(b) { var e, f, a = this, c = b.length, d = 0, h = a.i = a.j = a.m = 0; a.S = []; a.c = []; for (c || (b = [c++]); d < g;) a.S[d] = d++; for (d = 0; d < g; d++) e = a.S[d], h = h + e + b[d % c] & g - 1, f = a.S[h], a.S[d] = f, a.S[h] = e; a.g = function(b) { var c = a.S, d = a.i + 1 & g - 1, e = c[d], f = a.j + e & g - 1, h = c[f]; c[d] = h; c[f] = e; for (var i = c[e + h & g - 1]; --b;) d = d + 1 & g - 1, e = c[d], f = f + e & g - 1, h = c[f], c[d] = h, c[f] = e, i = i * g + c[e + h & g - 1]; a.i = d; a.j = f; return i }; a.g(g) } function p(b, e, f, a, c) { f = []; c = typeof b; if (e && c == "object") for (a in b) if (a.indexOf("S") < 5) try { f.push(p(b[a], e - 1)) } catch (d) {} return f.length ? f : b + (c != "string" ? "\0" : "") } function l(b, e, f, a) { b += ""; for (a = f = 0; a < b.length; a++) { var c = e, d = a & g - 1, h = (f ^= e[a & g - 1] * 19) + b.charCodeAt(a); c[d] = h & g - 1 } b = ""; for (a in e) b += String.fromCharCode(e[a]); return b } i.seedrandom = function(b, e) { var f = [], a; b = l(p(e ? [b, j] : arguments.length ? b : [(new Date).getTime(), j, window], 3), f); a = new q(f); l(a.S, j); i.random = function() { for (var c = a.g(m), d = o, b = 0; c < k;) c = (c + b) * g, d *= g, b = a.g(1); for (; c >= n;) c /= 2, d /= 2, b >>>= 1; return (c + b) / d }; return b }; o = i.pow(g, m); k = i.pow(2, k); n = k * 2; l(i.random(), j) })([], Math, 256, 6, 52);
+(function(j, i, g, m, k, n, o) { function q(b) { var e, f, a = this, c = b.length, d = 0, h = a.i = a.j = a.m = 0; a.S = []; a.c = []; for (c || (b = [c++]); d < g;) a.S[d] = d++; for (d = 0; d < g; d++) e = a.S[d], h = h + e + b[d % c] & g - 1, f = a.S[h], a.S[d] = f, a.S[h] = e; a.g = function(b) { var c = a.S, d = a.i + 1 & g - 1, e = c[d], f = a.j + e & g - 1, h = c[f]; c[d] = h; c[f] = e; for (var i = c[e + h & g - 1]; --b;) d = d + 1 & g - 1, e = c[d], f = f + e & g - 1, h = c[f], c[d] = h, c[f] = e, i = i * g + c[e + h & g - 1]; a.i = d; a.j = f; return i }; a.g(g) } function p(b, e, f, a, c) { f = []; c = typeof b; if (e && c == "object") for (a in b) if (a.indexOf("S") < 5) try { f.push(p(b[a], e - 1)) } catch (d) {} return f.length ? f : b + (c != "string" ? "\0" : "") } function l(b, e, f, a) { b += ""; for (a = f = 0; a < b.length; a++) { var c = e, d = a & g - 1, h = (f ^= e[a & g - 1] * 19) + b.charCodeAt(a); c[d] = h & g - 1 } b = ""; for (a in e) b += String.fromCharCode(e[a]); return b } i.seedrandom = function(b, e) { var f = [], a; b = l(p(e ? [b, j] : arguments.length ? b : [(new Date).getTime(), j, window], 3), f); a = new q(f); l(a.S, j); i.random = function() { for (var c = a.g(m), d = o, b = 0; c < k;) c = (c + b) * g, d *= g, b = a.g(1); for (; c >= n;) c /= 2, d /= 2, b >>>= 1; return (c + b) / d }; return b }; o = i.pow(g, m); k = i.pow(2, k); n = k * 2; l(i.random(), j) })([], Math, 256, 6, 52);
 // I don't know what the line above does, just copied and pasted it
 isATypeAlreadyUsed = function (goal, typesList) { // takes a goal and returns if one of his types is already in usedTypes
     var currentTypes = goal.types;
@@ -22,7 +22,7 @@ bingoGenerator = function (bingoList, opts) {
     var bingoBoard = [];
     var easyTiles = [3,4,7,10,11,15,17,19,21,23];//shaping the grid
     var mediumTiles = [1,5,8,9,12,14,16,18,22,25];
-    var hardTiles = [1,6,20,24];
+    var hardTiles = [2,6,20,24];
     var extraHardTiles = [13];
 
     var usedTypes = [];
@@ -30,7 +30,7 @@ bingoGenerator = function (bingoList, opts) {
     var randIndex = 0;
 
     for (var i = 1; i <= 25; i++) {
-        if (i in easyTiles) {
+        if (easyTiles.indexOf(i) > -1) {
             randIndex = Math.floor(Math.random() * bingoList[1].length);
             while (isATypeAlreadyUsed(bingoList[1][randIndex],usedTypes)) { //reroll until no type is in common
                 randIndex = Math.floor(Math.random() * bingoList[1].length);
@@ -40,7 +40,7 @@ bingoGenerator = function (bingoList, opts) {
             }
             k = k + (bingoList[1][randIndex].types).length; // moves where to add the new types
             bingoBoard[i] = bingoList[1][randIndex];
-        } else if (i in mediumTiles) {
+        } else if (mediumTiles.indexOf(i) > -1) {
             randIndex = Math.floor(Math.random() * bingoList[2].length);
             while (isATypeAlreadyUsed(bingoList[2][randIndex],usedTypes)) { //reroll until no type is in common
                 randIndex = Math.floor(Math.random() * bingoList[2].length);
@@ -50,7 +50,7 @@ bingoGenerator = function (bingoList, opts) {
             }
             k = k + (bingoList[2][randIndex].types).length; // moves where to add the new types
             bingoBoard[i] = bingoList[2][randIndex];
-        } else if (i in hardTiles) {
+        } else if (hardTiles.indexOf(i) > -1) {
             randIndex = Math.floor(Math.random() * bingoList[3].length);
             while (isATypeAlreadyUsed(bingoList[3][randIndex],usedTypes)) { //reroll until no type is in common
                 randIndex = Math.floor(Math.random() * bingoList[3].length);
