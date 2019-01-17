f97801ef7fcbaef7abbda5da5efb06721ed19933
Kyle Buzsaki <kbuzsaki@gmail.com>
2019-01-17 07:00:00 -0800

    Update Slime Rancher

--- bingosync-app/generators/slime_rancher_generator.js
+++ bingosync-app/generators/slime_rancher_generator.js
@@ -5,95 +5,98 @@ bingoList[1] = [
   { name: "Buy a Garden with all standard upgrades", types: [] }
 ];
 bingoList[2] = [
-  { name: "Have 3 Echoes of each color in your ranch", types: [] }
-];
-bingoList[3] = [
-  { name: "1 Hen / 1 Elder / 1 Stony / 1 Briar / 1 Painted Hens in the same Silo", types: [] },
   { name: "Buy Heart Module Mk III", types: [] }
 ];
-bingoList[4] = [
+bingoList[3] = [
   { name: "Buy a Coop with all standard upgrades", types: [] },
   { name: "Buy a Corral with all standard upgrades", types: [] },
-  { name: "Buy a Silo with all standard upgrades", types: [] }
+  { name: "Buy 10 Vacpack upgrades", types: [] }
+];
+bingoList[4] = [
+  { name: "Pop the Quantum Gordo", types: [] }
 ];
 bingoList[5] = [
+  { name: "1 Hen / 1 Elder / 1 Stony / 1 Briar / 1 Painted Hens in the same Silo", types: [] },
   { name: "Sell 20 Puddle Plorts", types: [] }
 ];
 bingoList[6] = [
-  { name: "Sell 10 Dervish Plorts", types: [] },
-  { name: "Buy 10 Vacpack upgrades", types: [] }
+  { name: "Pop the Rad Gordo", types: [] },
 ];
 bingoList[7] = [
-  { name: "Sell 10 Mosaic Plorts", types: [] },
-  { name: "Water 10 Oases", types: ["fountains"] }
+  { name: "Have 1 Coop, 1 Corral, 1 Garden, 1 Incinerator, 1 Pond, 1 Silo in your ranch", types: [] }
 ];
 bingoList[8] = [
-  { name: "Pop the Crystal Gordo", types: [] },
-  { name: "Have 1 Coop, 1 Corral, 1 Garden, 1 Incinerator, 1 Pond, 1 Silo in your ranch", types: [] },
-  { name: "Don't buy the Jetpack", types: ["restriction"] }
+  { name: "1 farm of each plantable fruit in your ranch", types: [] }
 ];
 bingoList[9] = [
-  { name: "1 farm of each plantable fruit in your ranch", types: [] },
-  { name: "Read \"Re: Africa\"", types: [] }
+  { name: "1 farm of each plantable veggie in your ranch", types: [] },
+  { name: "Pop the Dry Reef Tabby Gordo", types: [] }
 ];
 bingoList[10] = [
-  { name: "Sell 1 Gold Plort", types: [] },
-  { name: "1 farm of each plantable veggie in your ranch", types: [] }
-];
-bingoList[11] = [
-  { name: "1 Gilded Ginger", types: [] },
-  { name: "Don't buy Tank Booster Mk II", types: [] }
-];
-bingoList[12] = [
-  { name: "Pop the Dry Reef Tabby Gordo", types: [] },
+  { name: "Open the Ancient Gate", types: [] },
   { name: "You can only use a maximum of one Slime Key", types: [] }
 ];
+bingoList[11] = [
+  { name: "Pop the Crystal Gordo", types: [] },
+  { name: "Fill a Silo with 12 different foods", types: [] }
+];
+bingoList[12] = [
+  { name: "Pop 4 Gordos", types: ["gordos"] },
+  { name: "Don't buy Tank Booster", types: ["restriction"] }
+];
 bingoList[13] = [
-  { name: "Pop the Hunter Gordo", types: [] },
-  { name: "Read \"Re: Life is different now\"", types: [] }
+  { name: "Pop the Ring Island Pink Gordo", types: [] },
+  { name: "Sell 10 Tangle Plorts", types: [] }
 ];
 bingoList[14] = [
-  { name: "Pop the Ring Island Pink Gordo", types: [] },
-  { name: "Don't buy any Power Core upgrades", types: [] }
+  { name: "Pop the Boom Gordo", types: [] },
+  { name: "Don't buy any Power Core upgrades", types: ["restriction"] }
 ];
 bingoList[15] = [
-  { name: "Pop the Boom Gordo", types: [] },
-  { name: "Have 5 Corrals with completely different largos in each of them", types: [] }
-];
-bingoList[16] = [
-  { name: "Pop the Tangle Gordo", types: [] },
-  { name: "Complete 5 Range Exchange trades", types: ["rangeexchange"] }
-];
-bingoList[17] = [
-  { name: "Pop 7 Gordos", types: [] },
-  { name: "Don't vac up Pogofruits or Carrots", types: [] }
-];
-bingoList[18] = [
-  { name: "Activate 4 Desert Fountains", types: ["fountains"] }
-];
-bingoList[19] = [
-  { name: "Sell a non-Gold plort for over 120 newbucks", types: [] },
+  { name: "Pop 5 Gordos", types: ["gordos"] },
   { name: "Unlock the Overgrowth, the Grotto, and the Docks", types: [] }
 ];
+bingoList[16] = [
+  { name: "Unlock the Lab", types: ["lab"] },
+  { name: "1 Spicy Tofu", types: ["rangeexchange"] },
+  { name: "Have 5 Corrals with completely different largos in each of them", types: [] }
+];
+bingoList[17] = [
+  { name: "Pop 6 Gordos", types: ["gordos"] },
+  { name: "Pop the Hunter Gordo", types: [] },
+  { name: "Complete 5 Range Exchange trades", types: ["rangeexchange"] }
+];
+bingoList[18] = [
+  { name: "Water 10 Oases", types: ["fountains"] },
+  { name: "Fill a Silo with 12 different plorts", types: [] }
+];
+bingoList[19] = [
+  { name: "Don't vac up Pogofruits or Carrots", types: ["restriction"] },
+  { name: "Pop 7 Gordos", types: ["gordos"] },
+  { name: "1 Gilded Ginger", types: [] }
+];
 bingoList[20] = [
-  { name: "Unlock the Lab", types: [] }
+  { name: "Unlock Mochi's Extra Mile", types: ["rangeexchange"] },
+  { name: "Have 1 Echo of each color in your ranch", types: [] },
+  { name: "Activate 4 Desert Fountains", types: ["fountains"] }
 ];
 bingoList[21] = [
-  { name: "1 Spicy Tofu", types: ["rangeexchange"] },
-  { name: "Complete 10 Range Exchange trades", types: ["rangeexchange"] }
-];
-bingoList[22] = [
   { name: "Sell 1 Fire Plort", types: [] },
-  { name: "Create 5 total Apiarys, Drills, or Pumps", types: [] }
-];
-bingoList[23] = [
-  { name: "Pop the Dervish Gordo with Prickle Pears exclusively", types: [] },
   { name: "Have 4 Slime Toys in your ranch", types: [] }
 ];
+bingoList[22] = [
+  { name: "Pop 8 Gordos", types: ["gordos"] },
+  { name: "Pop the Dervish Gordo with Prickle Pears exclusively", types: [] },
+  { name: "Don't buy the Jetpack", types: ["restriction"] }
+];
+bingoList[23] = [
+  { name: "Pop the Tangle Gordo", types: [] },
+  { name: "Pop the Mosaic Gordo with Silver Parsnips exclusively", types: [] }
+];
 bingoList[24] = [
-  { name: "Pop the Mosaic Gordo with Silver Parsnips exclusively", types: [] },
-  { name: "Buy Treasure Cracker", types: [] }
+  { name: "Buy Treasure Cracker", types: ["lab"] },
+  { name: "Complete 10 Range Exchange trades", types: ["rangeexchange"] }
 ];
 bingoList[25] = [
-  { name: "Unlock Mochi's Extra Mile", types: ["rangeexchange"] }
+  { name: "Create 5 total Apiarys, Drills, or Pumps", types: ["lab"] }
 ];
