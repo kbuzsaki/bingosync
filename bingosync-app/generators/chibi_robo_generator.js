493f4d7ae49fcfd6d88f67cc85a957552f466512
Kyle Buzsaki <kbuzsaki@gmail.com>
2019-06-10 07:36:24 -0700

    Update Chibi-Robo

--- bingosync-app/generators/chibi_robo_generator.js
+++ bingosync-app/generators/chibi_robo_generator.js
@@ -1,63 +1,153 @@
-bingoGenerator = require("./generators/generator_bases/simple_generator.js");
+bingoGenerator = require("./generators/generator_bases/srl_generator_v5.js");
 
-var bingoList = [
-    "1000 Scrap",
-    "3 Frog Rings",
-    "400 Happy Points",
-    "4000 Moolah",
-    "500 Scrap",
-    "6 Candy Bags",
-    "6 Empty Cans",
-    "6 Frog Rings",
-    "700 Happy Points",
-    "8 Cookie Boxes",
-    "8000 Moolah",
-    "9 Frog Rings",
-    "All Chips",
-    "All Doors Backyard",
-    "All Doors Basement",
-    "All Doors Bedroom",
-    "All Doors Foyer",
-    "All Doors Jenny’s Room",
-    "All Doors Kitchen",
-    "All Doors Living Room",
-    "All Suits (Minus Tao and Super Chibi Robo)",
-    "All Tools",
-    "Bandage Delivery",
-    "Buy Chibi Battery",
-    "Buy Hot Rod",
-    "Buy Space Scrambler",
-    "Charge Giga Battery",
-    "Cooking Sticker",
-    "Deliver Photo",
-    "Deliver Pirate Ship",
-    "Deliver the Red Shoe",
-    "Divorce",
-    "Dog Tags",
-    "Drain Frog Ring",
-    "Fill Crayon Box",
-    "Free Funky Phil",
-    "Frog Sticker",
-    "Frog Suit",
-    "Give a Flower to Mom",
-    "Green Brick",
-    "Gunpowder",
-    "Mr Prongs Sticker",
-    "Mug",
-    "Obtain Radar",
-    "Obtain Spoon",
-    "Obtain Squirter",
-    "Open Bedroom Shortcut",
-    "Pajamas",
-    "Peekoe Sticker",
-    "Rail Training",
-    "Range Chip",
-    "Red Brick",
-    "Spy Training",
-    "Squirt Honey/Nectar on Sunshine",
-    "Ticket Stub Delivery",
-    "Utilibots Sticker",
-    "Wake up the Bird",
-    "Waterfall Frog Ring",
-    "White Brick"
+var bingoList = [];
+bingoList[1] = [
+  { name: "Make a Legendary Wooden Sword", types: [] },
+  { name: "Decorate a house with at least 3 pieces of furniture, outside the requirements", types: ["houses"] },
+  { name: "Wear Cactus Armor", types: [] },
+  { name: "Never upgrade your axe", types: ["restrictions"] },
+];
+bingoList[2] = [
+  { name: "Have 3 unpleasent Tools/Weapons", types: [] },
+  { name: "Fill the inventory of a Piggy Bank", types: [] },
+  { name: "Catch and contain 4 unique critters", types: [] },
+  { name: "Obtain a stack of 999 of any tile without duping", types: [] },
+];
+bingoList[3] = [
+  { name: "Have 3 different Watches", types: [] },
+  { name: "Fill an NPC house with bars", types: ["houses"] },
+  { name: "Wear a Ginger Beard", types: ["fishing"] },
+  { name: "Defeat King Slime", types: ["slime"] },
+];
+bingoList[4] = [
+  { name: "Have 5 different Pickaxes", types: [] },
+  { name: "Stay upside down in Space for 3 minutes", types: [] },
+  { name: "Find a Floating Island House, and a Floating Island Lake", types: ["island"] },
+  { name: "Have 2 different Spears", types: [] },
+];
+bingoList[5] = [
+  { name: "Defeat Eye of Cthulhu", types: ["eye"] },
+  { name: "Obtain 2 Zombie Banners", types: ["banners"] },
+  { name: "Defeat the Goblin Army", types: [] },
+  { name: "Wear a Cape", types: [] },
+];
+bingoList[6] = [
+  { name: "Collect all 5 unique Pre-HM Wooden Armor Sets", types: [] },
+  { name: "Wear Pumpkin Armor", types: [] },
+  { name: "Survive a Blood Moon", types: [] },
+  { name: "Wear an Umbrella Hat", types: ["rain"] },
+];
+bingoList[7] = [
+  { name: "Wear 5 different Accessories", types: [] },
+  { name: "Never have more than 4 NPCs", types: ["houses"] },
+  { name: "Wear a Rain Outfit", types: ["rain"] },
+  { name: "Obtain 100 Gel without duping", types: [] },
+];
+bingoList[8] = [
+  { name: "Make Sunglasses", types: [] },
+  { name: "Kill EoC using throwing weapons without explosives", types: ["eye"] },
+  { name: "Kill an NPC with the Dungeon Guardian", types: ["houses"] },
+  { name: "Defeat King Slime with True Melee", types: ["slime"] },
+];
+bingoList[9] = [
+  { name: "Defeat BoC/EoW", types: ["evilboss"] },
+  { name: "Never use an axe", types: ["restrictions"] },
+  { name: "Make a Surface Mushroom Biome", types: ["biomes"] },
+  { name: "Fish up 3 unique Crates", types: ["fishing"] },
+];
+bingoList[10] = [
+  { name: "Obtain 8 Unique Hooks", types: [] },
+  { name: "Obtain a Counterweight", types: [] },
+  { name: "Get 2 Summon Staves", types: [] },
+  { name: "Wear the Hero's Set", types: [] },
+];
+bingoList[11] = [
+  { name: "Mine an entire Floating Island", types: ["island"] },
+  { name: "Never use Torches", types: ["restrictions"] },
+  { name: "Never use Platforms", types: ["restrictions"] },
+  { name: "Make all the Gem Staves", types: [] },
+];
+bingoList[12] = [
+  { name: "Defeat Queen Bee", types: ["bee"] },
+  { name: "Have all 4 Strange Plants at once", types: ["dyes"] },
+  { name: "Fill your Dye slots with Strange Dyes", types: ["dyes"] },
+  { name: "Rescue the Stylist", types: [] },
+];
+bingoList[13] = [
+  { name: "Drain Both Oceans into the Underworld", types: [] },
+  { name: "Make a Mana Flower", types: [] },
+  { name: "Never make NPC Houses", types: ["houses","restrictions"] },
+  { name: "Never use a Hook", types: ["restrictions"] },
+];
+bingoList[14] = [
+  { name: "Mine an Entire Meteor", types: [] },
+  { name: "Accrue 2 Platinum Coins without duping", types: [] },
+  { name: "Make a Horseshoe Balloon", types: [] },
+  { name: "Make a GPS", types: [] },
+];
+bingoList[15] = [
+  { name: "Beat Old One's Army Tier 1", types: ["ooa"] },
+  { name: "Make the following Biome: Jungle Ocean", types: ["biomes"] },
+  { name: "Make the following Biome: Evil Ocean", types: ["biomes"] },
+  { name: "Kill BoC/EoW with the evil biome gun", types: ["evilboss"] },
+];
+bingoList[16] = [
+  { name: "Obtain 3 different Slime Banners", types: ["banners"] },
+  { name: "Make the Star Cannon", types: [] },
+  { name: "Fish up a Reaver Shark, Sawtooth Shark, and a Rock Fish", types: ["fishing"] },
+  { name: "Make Diving Gear", types: [] },
+];
+bingoList[17] = [
+  { name: "Defeat Skeletron", types: ["skeletron"] },
+  { name: "Make the following Biome: Ice Jungle", types: ["biomes"] },
+  { name: "Open 3 Golden Chests", types: ["fishing"] },
+  { name: "Defeat EoC and Queen Bee at the same time", types: ["eye","bee"] },
+];
+bingoList[18] = [
+  { name: "Obtain 3 Different Sentry Rods", types: ["ooa"] },
+  { name: "Kill the Clothier", types: [] },
+  { name: "Make a Fart in a Bottle", types: [] },
+  { name: "Purchase the Magic Lantern", types: [] },
+];
+bingoList[19] = [
+  { name: "Have 2 different Flails", types: [] },
+  { name: "Make the following Biome: Evil Jungle", types: ["biomes"] },
+  { name: "Have 12 different Buffs/Debuffs", types: [] },
+  { name: "Have 55 Defense", types: [] },
+];
+bingoList[20] = [
+  { name: "Obtain 3 different Boss Masks", types: [] },
+  { name: "Obtain a Balloon Animal", types: [] },
+  { name: "Purify your world until there is less than 5% world evil", types: [] },
+  { name: "Kill Skeletron during the day", types: ["skeletron"] },
+];
+bingoList[21] = [
+  { name: "Open 3 Shadow Chests", types: [] },
+  { name: "Have 10 NPCs", types: ["houses"] },
+  { name: "Have 2 different Mounts", types: [] },
+  { name: "Obtain a Demon Scythe", types: [] },
+];
+bingoList[22] = [
+  { name: "Collect 2 Guide Voodoo Dolls", types: [] },
+  { name: "Kill unique bosses with each of the following: Melee, Ranged, Mage, Summoner", types: [] },
+  { name: "Have 3 different Pets", types: [] },
+  { name: "Place 3 Boss Trophies", types: [] },
+];
+bingoList[23] = [
+  { name: "Make a Goblin Tech", types: [] },
+  { name: "Kill EoC with True Melee", types: ["eye"] },
+  { name: "Have 5 Different Yo-Yos", types: [] },
+  { name: "Obtain both the Bride and Groom sets", types: [] },
+];
+bingoList[24] = [
+  { name: "Make a REK 3000", types: [] },
+  { name: "Make the Rainbow String", types: ["dyes"] },
+  { name: "Make Frostspark Boots", types: [] },
+  { name: "Never use Accessories", types: ["restrictions"] },
+];
+bingoList[25] = [
+  { name: "Defeat WoF", types: [] },
+  { name: "Wear the Plumbers Set", types: [] },
+  { name: "Wear the Bee Set, Honeycomb, Hive Wand, and Bee Mount at the same time", types: [] },
+  { name: "Obtain 8 unique Banners", types: ["banners"] },
 ];
