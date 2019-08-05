3a4c8e06db361ac451470813d1a6d6df7ac01991
Kyle Buzsaki <kbuzsaki@gmail.com>
2019-08-05 07:52:40 -0700

    Update hat in time

--- bingosync-app/generators/hat_in_time_generator.js
+++ bingosync-app/generators/hat_in_time_generator.js
@@ -3,14 +3,11 @@ var bingoGenerator = require("./generators/generator_bases/srl_generator_v5.js")
 var bingoList = [];
 
 bingoList[1] = [
-  { name: "Craft 2 Hats", types: ["hat"] },
   { name: "Collect 1 Roulette Token", types: ["token"] },
   { name: "Open 4 Chests in Mafia Town", types: ["mafia"] },
-  { name: "Activate and Complete 2 Pon Party Balls", types: ["pon"] },
   { name: "Open the Red Mafia Vault" , types: ["mafia"] },
 ];
 bingoList[2] = [
-  { name: "Rumbi Selfie", types: ["selfie"] },
   { name: "Buy 1 Badge", types: ["badge"] },
   { name: "Open the Blue Mafia Vault" , types: ["mafia"] },
 ];
@@ -32,26 +29,20 @@ bingoList[5] = [
 bingoList[6] = [
   { name: "Repair 1 Relic" , types: ["relic"] },
   { name: "Collect 3 Roulette Tokens", types: ["token"] },
-  { name: "Collect 3 Yarns in Subcon", types: ["subcon"] },
+  { name: "Collect 5 Yarns in Subcon", types: ["subcon"] },
   { name: "Open the Green Mafia Vault" , types: ["mafia"] },
 ];
 bingoList[7] = [
-  { name: "Craft the Ice Hat", types: ["hats"] },
   { name: "Get 2 Time Pieces from Time Rifts", types: ["rift"] },
-  { name: "Repair 2 Relics", types: ["relic"] },
-  { name: "Kill 6 Sleepy Racoons", types: ["mafia"] },
 ];
-
-
 bingoList[8] = [
   { name: "Complete 1 Pink Time Rift", types: ["rift"] },
   { name: "Collect all the Photo Album pieces from Mafia of Cooks", types: ["rift"] },
-  { name: "Collect all comic pieces from one pink time rift", types: ["rift"] },
 ];
 bingoList[9] = [
-  { name: "Grab all 4 golden tickets in Golden Vault", types: ["act"] },
-  { name: "Complete all 4 endorsements in Picture Perfect", types: ["act"] },
+  { name: "Complete all 4 endorsements in Picture Perfect", types: ["botb"] },
   { name: "Hit DJ Grooves in Three Different Places in Dead Bird Studio", types: ["botb"] },
+  { name: "Repair 2 Relics", types: ["relic"] },
 ];
 bingoList[10] = [
   { name: "Complete 2 Pink Rifts", types: ["rift"] },
@@ -62,66 +53,59 @@ bingoList[11] = [
   { name: "Get 4 Time Pieces from Time Rifts", types: ["rift"] },
   { name: "Open 3 chests in Dead Bird Studio", types: ["botb"] },
   { name: "Hit The Conductor in Three Different Places in Dead Bird Studio", types: ["botb"] },
-  { name: "Repair 3 Relics", types: ["relic"] },
 ];
 bingoList[12] = [
   { name: "Get all photo album pieces from DBS Rift", types: ["rift"] },
   { name: "Selfie at the top of floating stonehenge", types: ["subcon"] },
-  { name: "Solve the Murder on the Owl Express", types: ["botb"] },
+  { name: "Complete the Murder on the Owl Express", types: ["botb"] },
 ];
 bingoList[13] = [
   { name: "Storytime Selfie", types: ["subcon"] },
   { name: "Collect 8 Relics", types: ["relic"] },
 ];
 bingoList[14] = [
-   { name: "Vanessa Selfie", types: ["subcon"] },
-  { name: "Get all 10 Time Pieces from Mafia Town", types: ["mafia"] },
+  { name: "The Village Rift", types: ["subcon"] },
 ];
 bingoList[15] = [
-  { name: "Destroy 3 tracker eyeballs in Subcon forest", types: ["subcon"] },
-  { name: "Owl Express Rift", types: ["rift"] },
-  { name: "Train rush with 100+ seconds left", types: ["botb"] },
-   { name: "Beat 2 Bosses", types: ["boss"] },
   { name: "Gather 3 Pieces of Evidence in Murder on the Owl Express", types: ["botb"] },
 ];
-
-
-
 bingoList[16] = [
-  { name: "Complete 3 Painting Burning Rituals in Subcon", types: ["subcon"] },
+  { name: "Vanessa Selfie", types: ["subcon"] },
   { name: "Collect 5 Roulette Tokens", types: ["token"] },
-  { name: "Craft 4 Hats", types: ["hat"] },
-  { name: "Collect 12 Relics", types: ["relic"] },
 ];
 bingoList[17] = [
-  { name: "Beat any Finale Excluding Fake Finale", types: ["boss"] },
-  { name: "Kill the Toilet of Doom", types: ["boss"] },
+  { name: "Complete 3 Painting Burning Rituals in Subcon", types: ["subcon"] },
+  { name: "Create a Relic in the Gallery", types: [""] },
+  { name: "Parade Selfie", types: ["botb"] },
 ];
 bingoList[18] = [
-  { name: "Collect all photo album pieces from Sleepy Subcon rift", types: ["rift"] },
-  { name: "Create a Relic in The Lab", types: ["relic"] },
+  { name: "Owl Express Rift", types: ["botb"] },
+  { name: "Train rush with 100+ seconds left", types: ["botb"] },
 ];
 bingoList[19] = [
-  { name: "Ring the Bell to Open the Gate in Alpine Skyline", types: ["alpine"] },
+  { name: "Destroy 3 tracker eyeballs in Subcon forest", types: ["subcon"] },
+  { name: "Collect 10 Relics", types: ["relic"] },
 ];
 bingoList[20] = [
-  { name: "Defeat Snatcher", types: ["subcon"] },
-  { name: "Get the Time Piece from The Lab Rift", types: ["rift"] },
+  { name: "Cheat the Race", types: ["mafia"] },
+  { name: "Craft 4 hats", types: ["hat"] },
 ];
 bingoList[21] = [
-  { name: "Collect 15 Time Pieces", types: ["tp"] },
-  { name: "Selfie with a goat", types: ["alpine"] },
+  { name: "Repair 3 Relics", types: ["relic"] },
+  { name: "Collect 13 Relics", types: ["relic"] },
 ];
 bingoList[22] = [
-  { name: "Reach one of the Prayer Flags atop Goat Village", types: ["alpine"] },
+  { name: "Get all 10 Time Pieces from Mafia Town", types: ["mafia"] },
+  { name: "Collect 7 Roulette Tokens", types: ["token"] },
 ];
 bingoList[23] = [
-  { name: "Get the chest at the Goat Refinery", types: ["alpine"] },
-  { name: "Blow the Horn at Goat Outpost", types: ["alpine"] },
+  { name: "Complete the Big Parade", types: ["botb"] },
+  { name: "Kill the Toilet of Doom", types: ["boss"] },
 ];
 bingoList[24] = [
-  { name: "Blow the Horn in Mystifying Time Mesa", types: ["alpine"] },
+  { name: "Defeat Snatcher", types: ["subcon"] },
 ];
 bingoList[25] = [
-  { name: "Get the chest at Purrloined Village", types: ["alpine"] },
+  { name: "Complete the Battle of the Birds Award Ceremony", types: ["botb"] },
+  { name: "Beat 3 Bosses", types: ["Boss"] },
 ];
