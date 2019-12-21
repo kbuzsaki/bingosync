5037b9a15f515eaf20095f592cebd7b304d43a07
Kyle Buzsaki <kbuzsaki@gmail.com>
2019-12-21 13:58:16 -0800

    Add tests for basic custom generator

--- bingosync-app/bingosync/test_bingosync.py
+++ bingosync-app/bingosync/test_bingosync.py
@@ -9,6 +9,9 @@ MAKE_ROOM_BUTTON = '<input type="submit" class="form-control" value="Make Room" 
 JOIN_ROOM_BUTTON = '<input type="submit" class="form-control" value="Join Room" />'
 BOARD_CONTAINER_HTML = '<div class="board-container">'
 
+def filter_keys(d, keys):
+    return dict((k, v) for k, v in d.items() if k in keys)
+
 class HomeTestCase(test.TestCase):
 
     def setUp(self):
@@ -58,6 +61,69 @@ class HomeTestCase(test.TestCase):
         room_td = '<td><a href="' + room.get_absolute_url() + '">Test Room</a></td>'
         self.assertContains(resp, room_td)
         self.assertContains(resp, MAKE_ROOM_BUTTON, html=True)
+
+class CustomTestCase(test.TestCase):
+
+    def setUp(self):
+        self.numeric_goals = [
+            {"name": "goal 1" }, {"name": "goal 2" }, {"name": "goal 3" }, {"name": "goal 4" }, {"name": "goal 5" },
+            {"name": "goal 6" }, {"name": "goal 7" }, {"name": "goal 8" }, {"name": "goal 9" }, {"name": "goal 10"},
+            {"name": "goal 11"}, {"name": "goal 12"}, {"name": "goal 13"}, {"name": "goal 14"}, {"name": "goal 15"},
+            {"name": "goal 16"}, {"name": "goal 17"}, {"name": "goal 18"}, {"name": "goal 19"}, {"name": "goal 20"},
+            {"name": "goal 21"}, {"name": "goal 22"}, {"name": "goal 23"}, {"name": "goal 24"}, {"name": "goal 25"}]
+
+    def make_room_with_custom_json(self, **overrides):
+        args = {
+            "room_name": "Test Room",
+            "passphrase": "password",
+            "nickname": "Bingoer",
+            "game_type": str(models.GameType.custom.value),
+            "lockout_mode": str(models.LockoutMode.lockout.value),
+            "custom_json": "",
+        }
+        args.update(overrides)
+        custom_room_form = forms.RoomForm(args)
+
+        return self.client.post("/", custom_room_form.data, follow=True)
+
+    def test_basic_custom(self):
+        custom_goals = self.numeric_goals
+        create_resp = self.make_room_with_custom_json(custom_json=json.dumps(custom_goals))
+        game = create_resp.context["room"].current_game
+
+        board_goals = [filter_keys(square, ["name"]) for square in game.board]
+        self.assertEqual(board_goals, custom_goals)
+
+    def test_invalid_json(self):
+        create_resp = self.make_room_with_custom_json(custom_json="foo")
+        self.assertContains(create_resp, "Invalid Board Json")
+
+    def test_invalid_not_list(self):
+        create_resp = self.make_room_with_custom_json(custom_json='{"some_dict": "value"}')
+        self.assertContains(create_resp, "Board must be a list")
+
+    def test_invalid_length(self):
+        custom_goals = list(self.numeric_goals)
+        del custom_goals[-1]
+        create_resp = self.make_room_with_custom_json(custom_json=json.dumps(custom_goals))
+        self.assertContains(create_resp, "Invalid board length 24, expected 25")
+
+    def test_invalid_missing_name(self):
+        # pretend there's a typo
+        custom_goals = list(self.numeric_goals)
+        del custom_goals[2]["name"]
+        custom_goals[2]["naem"] = "foo"
+
+        create_resp = self.make_room_with_custom_json(custom_json=json.dumps(custom_goals))
+        self.assertContains(create_resp, "Square 3 ({&quot;naem&quot;: &quot;foo&quot;}) is missing a &quot;name")
+
+    def test_invalid_empty_name(self):
+        # pretend there's an empty name
+        custom_goals = list(self.numeric_goals)
+        custom_goals[2]["name"] = ""
+
+        create_resp = self.make_room_with_custom_json(custom_json=json.dumps(custom_goals))
+        self.assertContains(create_resp, "Square 3 ({&quot;name&quot;: &quot;&quot;}) has an empty &quot;name")
 
 class ApiTestCase(test.TestCase):
 
