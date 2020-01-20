e66dcc2f7de1a92f2529561a138b425120d01af4
Kyle Buzsaki <kbuzsaki@gmail.com>
2020-01-20 08:05:35 -0800

    Break chat panel into a separate module, add most of its tests

--- bingosync-app/static/tests/qunit-extensions.js
+++ bingosync-app/static/tests/qunit-extensions.js
@@ -10,6 +10,23 @@
         };
     };
 
+    QUnit.assert.elementsAre = function(actuals, assertion, expecteds) {
+        var assert = this;
+        assert.strictEqual(actuals.length, expecteds.length);
+        for (var i = 0; i < actuals.length && i < expecteds.length; i++) {
+            assertion(actuals[i], expecteds[i]);
+        }
+    };
+
+    QUnit.assert.outerHtmlEquals = function(actual, expected) {
+        var assert = this;
+        var actualHtml = (actual.outerHTML) ? actual.outerHTML : actual;
+        var expectedHtml = (expected.outerHTML) ? expected.outerHTML : expected;
+        assert.strictEqual(actualHtml, expectedHtml);
+    };
+
     // speed up tests by default by reducing responseTime to 10ms from 500ms
     $.mockjaxSettings.responseTime = 10;
+    // print deeper diffs
+    QUnit.dump.maxDepth = 10;
 })();
