0bd7bb22288a7c66cf9bac14b558ea6edc2eb1de
Kyle Buzsaki <kbuzsaki@gmail.com>
2020-03-29 13:00:06 -0700

    Use .format for CustomGenerator error messages instead of string concatenation

--- bingosync-app/bingosync/generators/custom_generator.py
+++ bingosync-app/bingosync/generators/custom_generator.py
@@ -13,23 +13,23 @@ class InvalidBoardException(Exception):
 
 def _validate_square(i, square):
     if "name" not in square:
-        raise InvalidBoardException("Square " + str(1 + i)
-                + " (" + json.dumps(square) + ") is missing a \"name\" attribute")
+        raise InvalidBoardException(
+                'Square {} ({}) is missing a "name" attribute'.format(i + 1, json.dumps(square)))
     elif square["name"] == "":
-        raise InvalidBoardException("Square " + str(1 + i)
-                + " (" + json.dumps(square) + ") has an empty \"name\" attribute")
+        raise InvalidBoardException(
+                'Square {} ({}) has an empty "name" attribute'.format(i + 1, json.dumps(square)))
 
 
 def _parse_simple_list(custom_board, game_type):
     if not isinstance(custom_board, list):
-        raise InvalidBoardException("Board must be a list")
+        raise InvalidBoardException('Board must be a list')
 
     if game_type == GameType.custom and len(custom_board) != 25:
-        raise InvalidBoardException("A fixed board must have exactly 25 goals (found "
-                + str(len(custom_board)) + ")")
+        raise InvalidBoardException(
+                'A fixed board must have exactly 25 goals (found {})'.format(len(custom_board)))
     elif game_type == GameType.custom_randomized and len(custom_board) < 25:
-        raise InvalidBoardException("A randomized board must have at least 25 goals (found "
-                + str(len(custom_board)) + ")")
+        raise InvalidBoardException(
+                'A randomized board must have at least 25 goals (found {})'.format(len(custom_board)))
 
     for i, square in enumerate(custom_board):
         _validate_square(i, square)
@@ -41,19 +41,20 @@ class CustomGenerator:
 
     def __init__(self, game_type):
         if not game_type.is_custom:
-            raise Exception("Tried to instantiate CustomGenerator with invalid GameType: " + str(game_type))
+            raise Exception(
+                    'Tried to instantiate CustomGenerator with invalid GameType: {}'.format(game_type))
         self.game_type = game_type
 
     def validate_custom_json(self, custom_json):
         try:
             custom_board = json.loads(custom_json)
         except json.decoder.JSONDecodeError as e:
-            raise InvalidBoardException("Invalid Board Json")
+            raise InvalidBoardException('Invalid Board Json')
 
         if self.game_type in (GameType.custom, GameType.custom_randomized):
             return _parse_simple_list(custom_board, self.game_type)
 
-        raise Exception("Unrecognized custom game type: " + str(self.game_type))
+        raise Exception('Unrecognized custom game type: {}'.format(self.game_type))
 
     def get_card(self, seed, custom_board):
         if self.game_type == GameType.custom:
