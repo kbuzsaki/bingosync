bbcc49fd16831f09fb746dce1132cd3eeff86276
Kyle Buzsaki <kbuzsaki@gmail.com>
2020-08-17 18:42:32 -0700

    Update Monster Rancher 2 bingo

--- bingosync-app/generators/monster_rancher_2_generator.js
+++ bingosync-app/generators/monster_rancher_2_generator.js
@@ -4,150 +4,175 @@ var bingoList = [];
 bingoList[1] = [
  {name: "Feed Monster 12 Potatoes", types: ["food"] },
  {name: "Unlock FIMBA Monsters", types: ["quick","unlock"] },
- {name: "Move Misses At 95% Or More", types: ["battles","rng"] },
+ {name: "Move Misses At 95% Or More", types: ["fightrng"] },
  {name: "Two Gemini Pots In Inventory", types: ["battles","items"]},
+ {name: "+600 In Two Stats", types: ["stats"] },
 ];
 bingoList[2] = [
- {name: "Go Bankrupt", types: ["money","quick"] },
+ {name: "Go Bankrupt", types: ["money"] },
  {name: "Monster Dies At < 1 Year Old", types: ["death","quick"] },
- {name: "1 Hit KO Opponent", types: ["rng","battles"] },
+ {name: "1 Hit KO Opponent", types: ["fight"] },
  {name: "Unlock 15 Monster Cards", types: ["cards","combine"]},
+ {name: "Defeat 2 Different Phoenixs", types: ["battles"] },
 ];
 bingoList[3] = [
- {name: "Fight King Ape", types: ["long","errantry"] },
+ {name: "Fight King Ape", types: ["long"] },
  {name: "Win A Tournament With AI Control Only", types: ["battles"] },
- {name: "Discard Pure Platina", types: ["items"] },
+ {name: "Move Hits At 10% Or Less", types: ["fightrng"] },
  {name: "Fill Inventory With Smoked Snakes", types: ["shop","items"]},
+ {name: "500+ Life", types: ["stats"] },
 ];
 bingoList[4] = [
- {name: "Move Hits At 10% Or Less", types: ["battles","rng"] },
+ {name: "Discard Pure Platina", types: ["items"] },
  {name: "Rewarded 3000G On 1 Errantry", types: ["errantry"] },
  {name: "Unlock 10 Monster Cards", types: ["cards","combine"] },
- {name: "2 Crits Back To Back", types: ["battles","rng"] },
+ {name: "2 Crits Back To Back", types: ["fightrng"] },
+ {name: "Defeat 3 Different Hares", types: ["battles"] },
 ];
 bingoList[5] = [
  {name: "Win 8 Tournaments", types: ["battles"] },
  {name: "Use 3 Drugs On 1 Monster", types: ["drugs"] },
  {name: "Finish 1 Tech Chain", types: ["techs","errantry"] },
- {name: "Defeat 3 Different Mocchis", types: ["battles"] },
+ {name: "Activate Ease", types: ["fightrng","specials"] },
+ {name: "Boots And Big Footprint In Inventory", types: ["items"] },
 ];
 bingoList[6] = [
- {name: "KO Oakleyman", types: ["battles"] },
+ {name: "KO Oakleyman", types: ["fight"] },
  {name: "Spend Over 10000G At The Shop In 1 Trip", types: ["shop"] },
  {name: "Strong Glue", types: ["expedition"] },
- {name: "Defeat 3 Different Suezos", types: ["battles"] },
+ {name: "5 Different Disc Chips In Inventory", types: ["battles","items"] },
+ {name: "KO Opponent In 4 Seconds Or Less", types: ["fight"] },
 ];
 bingoList[7] = [
  {name: "Find Kato Disc Chips On Expedition", types: ["items","expedition"] },
  {name: "Unlock Dragon", types: ["unlock","long"] },
- {name: "Activate Ease", types: ["battles","specials"] },
- {name: "Have 1 Tech In Each Slot", types: ["battles"] },
+ {name: "Truancy", types: ["quick","ranch"] },
+ {name: "Wins/Losses Mirrored On Grid", types: ["rng","battles"] },
+ {name: "999 Damage", types: ["fight"] },
 ];
 bingoList[8] = [
  {name: "Find Two Pure Gold On 1 Expedition", types: ["expedition"] },
- {name: "Activate Fury", types: ["battles","specials"] },
+ {name: "Activate Fury", types: ["fightrng","specials"] },
  {name: "Lose A Week Of Life With A Banana", types: ["items"] },
  {name: "Monster Card Ending In 1", types: ["cards"] },
+ {name: "Win 3 F-Rank Tournaments", types: ["battles"] },
 ];
 bingoList[9] = [
- {name: "Monster Flattened In Battle", types: ["battles"] },
+ {name: "Monster Flattened In Battle", types: ["fight"] },
  {name: "2 Greats In A Row", types: ["rng","ranch"] },
  {name: "2 Crab’s Claws In Inventory", types: ["battles","items"] },
- {name: "Two Statues In Inventory", types: ["battles","items"]},
+ {name: "Have 1 Tech In Each Slot", types: ["errantry"] },
+ {name: "Feed Monster 12 Tablets", types: ["food"] },
 ];
 bingoList[10] = [
  {name: "Beat Your Monster In Sumo", types: ["play","ranch"] },
- {name: "Run Away", types: ["ranch"] },
- {name: "Grit And Win", types: ["rng","battles","specials"] },
- {name: "0 to 99 Guts", types: ["battles"] },
+ {name: "Get 5 Diamond Marks", types: ["food"] },
+ {name: "Grit And Win", types: ["fightrng","specials"] },
+ {name: "Two Statues In Inventory", types: ["battles","items"]},
+ {name: "8 Monsters Spell Trainer Name In Freezer", types: ["freezer"] },
 ];
 bingoList[11] = [
  {name: "Unlock Ghost", types: ["unlock","death","long"] },
  {name: "Refuse 1 Ranch Upgrade", types: ["ranch"] },
- {name: "Wins/Losses Mirrored On Grid", types: ["rng","battles"] },
- {name: "Truancy", types: ["quick","ranch"] },
+ {name: "0 to 99 Guts", types: ["fight"] },
+ {name: "Defeat 3 Different Mocchis", types: ["battles"] },
+ {name: "Zuum Disc Chips And Dino Tail In Inventory", types: ["items"] },
 ];
 bingoList[12] = [
  {name: "Mock Seeds", types: ["long","shop","unlock"] },
- {name: "Get 5 Diamond Marks", types: ["food"] },
- {name: "Use A Move That Costs 50 Guts Or More", types: ["battles"] },
+ {name: "Use A Move That Costs 50 Guts Or More", types: ["fight"] },
  {name: "Defeat 4 Different Zuums", types: ["battles"] },
+ {name: "Run Away", types: ["quick","ranch"] },
+ {name: "Plant Chips And Flower In Inventory", types: ["items"] },
 ];
 bingoList[13] = [
- {name: "Commit Foolery 3 Times In One Battle", types: ["battles"] },
+ {name: "Commit Foolery 3 Times In One Battle", types: ["fightrng"] },
  {name: "Unlock One Move From Each Errantry", types: ["errantry"] },
  {name: "5 Or More Funeral Attendees", types: ["death"] },
- {name: "Commit Foolery 6 Times In One Battle", types: ["battles"] },
+ {name: "Commit Foolery 6 Times In One Battle", types: ["fightrng"] },
+ {name: "Defeat 3 Different Suezos", types: ["battles"] },
 ];
 bingoList[14] = [
- {name: "0 Funeral Attendees", types: ["quick","death"] },
+ {name: "Unlock Joker", types: ["unlock","expedition"] },
  {name: "Failure 3 Times In A Row", types: ["ranch","quick"] },
- {name: "Beat Your Monster In Mudball Fight", types: ["play","ranch"] },
- {name: "1 Damage", types: ["quick","battles"] },
+ {name: "1 Damage", types: ["fight"] },
+ {name: "3 \"I Regret…\"s At The Shrine", types: ["rng"] },
+ {name: "6 \"I Regret…\"s At The Shrine", types: ["rng"] },
 ];
 bingoList[15] = [
- {name: "Unlock Joker", types: ["unlock","expedition"] },
+ {name: "Beat Your Monster In Mudball Fight", types: ["play","ranch"] },
  {name: "50000G", types: ["money"] },
- {name: "Wither For 60+ Guts", types: ["techs","battles"] },
- {name: "Crit 1 Damage", types: ["quick","battles"] },
+ {name: "Unlock Centaur", types: ["unlock"] },
+ {name: "Crit 1 Damage", types: ["fightrng"] },
+ {name: "0 Funeral Attendees", types: ["quick","death"] },
 ];
 bingoList[16] = [
  {name: "100 Fame", types: ["battles","stats"] },
- {name: "Don’t Visit Aunt Verde For 1 Year", types: ["quick","shop"] },
- {name: "Unlock Centaur", types: ["unlock"] },
+ {name: "Scold Monster For Cheating", types: ["quick","ranch"] },
  {name: "Rewarded Money On Expedition", types: ["expedition"] },
+ {name: "Wither For 60+ Guts", types: ["techs","fight"] },
+ {name: "Monster Gets +1 On Two Different Drills", types: ["combination"] },
 ];
 bingoList[17] = [
- {name: "Activate Fight", types: ["battles","specials"] },
+ {name: "Activate Fight", types: ["fightrng","specials"] },
  {name: "Spartan", types: ["stats","ranch"] },
- {name: "Scold Monster For Cheating", types: ["quick","ranch"] },
- {name: "Activate Unity", types: ["specials","battles"] },
+ {name: "Don’t Visit Aunt Verde For 1 Year", types: ["quick","shop"] },
+ {name: "Find Phoenix Feather On First Expedition", types: ["expedition"] },
+ {name: "Monster Gets +1 On Errantry", types: ["combination"] },
 ];
 bingoList[18] = [
- {name: "Find Phoenix Feather On First Expedition", types: ["expedition"] },
+ {name: "Activate Unity", types: ["specials","fightrng"] },
  {name: "Unlock 3 Enemy Cards", types: ["cards"] },
  {name: "Beat Your Monster In Sparring", types: ["play","ranch"] },
- {name: "\"It’s Not A Serious Injury\"", types: ["ranch"] },
+ {name: "\"It’s Not A Serious Injury\"", types: ["harmful"] },
+ {name: "Sell Monster For At Least 50G", types: ["quick","market"] },
 ];
 bingoList[19] = [
- {name: "Breeder Rank 7", types: ["long"] },
- {name: "Activate Will", types: ["battles","specials"] },
+ {name: "Breeder Rank 7", types: ["long","battles"] },
+ {name: "Activate Will", types: ["fightrng","specials"] },
  {name: "Refuse Praise For Monster Giving Item", types: ["quick","items","ranch"] },
- {name: "Errow Smiles", types: ["errantry"] },
+ {name: "Don’t See Errow Until 1002", types: ["errantry"] },
+ {name: "Sell Monster For At Least 1000G", types: ["long","market"] },
 ];
 bingoList[20] = [
- {name: "Win A Fight Using Only Recovery Moves", types: ["battles","techs"] },
+ {name: "Win A Fight Using Only Recovery Moves", types: ["fight","techs"] },
  {name: "Try To Use Clay Doll As Combining Item", types: ["items","combine"] },
  {name: "Every Stat Ends With 1", types: ["stats"] },
  {name: "Defeat A Jill", types: ["battles"] },
+ {name: "Bad To Good Nature", types: ["ranch"] },
 ];
 bingoList[21] = [
- {name: "Power And Win", types: ["rng","battles","specials"] },
+ {name: "Power And Win", types: ["fightrng","specials"] },
  {name: "Unlock Durahan", types: ["unlock","expedition"] },
  {name: "Never See Kavaro", types: ["expedition"] },
- {name: "Monster(s) Give 3 Different Items", types: ["rng","items"] },
+ {name: "Dig Up Hotsprings", types: ["monspec","ranch"] },
+ {name: "Search Elephant Statue", types: ["expedition"] },
 ];
 bingoList[22] = [
- {name: "Dig Up Hotsprings", types: ["monspec","ranch"] },
+ {name: "Monster(s) Give 3 Different Items", types: ["rng","items"] },
  {name: "Beat Your Monster In Snowball Fight", types: ["play","ranch"] },
- {name: "Activate Guard", types: ["battles","specials"] },
- {name: "KO Waranmo", types: ["battles"] },
+ {name: "Activate Guard", types: ["fightrng","specials"] },
+ {name: "Unlock 20 Monster Cards", types: ["cards","combine"] },
+ {name: "Search Jill Statue", types: ["expedition"] },
 ];
 bingoList[23] = [
- {name: "IMa/FIMBA Sweep", types: ["battles"] },
+ {name: "Win More Than 2000G At IMa/FIMBA", types: ["battles"] },
  {name: "Golem Or Baku On Ranch", types: ["monspec","ranch"] },
- {name: "Feed Monster 12 Milk", types: ["food"] },
- {name: "Get KO’d By Tiravail", types: ["battles"] },
+ {name: "800+ Intelligence", types: ["stats"] },
+ {name: "Get KO’d By Tiravail", types: ["fight"] },
+ {name: "Give Monster 1 Of Each Shop Item Before 1001", types: ["items","shop"] },
 ];
 bingoList[24] = [
  {name: "Fill Freezer With Different Monsters", types: ["combine"] },
  {name: "800+ Speed", types: ["stats"] },
- {name: "Unlock 20 Monster Cards", types: ["cards","combine"] },
+ {name: "KO Waranmo", types: ["fight"] },
  {name: "Colt Discreetly Throws Away Item", types: ["items"] },
+ {name: "Win By Less Than 1%", types: ["fightrng"] },
 ];
 bingoList[25] = [
  {name: "800+ Power", types: ["stats"] },
- {name: "800+ Intelligence", types: ["stats"] },
+ {name: "Feed Monster 12 Milk", types: ["food"] },
  {name: "Unlock Phoenix", types: ["unlock","expedition"] },
- {name: "Kitty’s Bang Doesn't KO", types: ["battles"] },
+ {name: "Kitty’s Bang Doesn’t KO", types: ["fight"] },
+ {name: "Naga Or Monol On Ranch", types: ["ranch","monspec"] },
 ];
