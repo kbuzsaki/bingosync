5ba62eced867865e75f5560c2edf2e97f6543f02
Kyle Buzsaki <kbuzsaki@gmail.com>
2021-04-14 00:07:16 -0700

    Update Hades

--- bingosync-app/generators/hades_generator.js
+++ bingosync-app/generators/hades_generator.js
@@ -2,25 +2,25 @@ bingoGenerator = require("./generators/generator_bases/simple_generator.js");
 
 var bingoList = [
     {
-        "name": "2 wins in a row"
+        "name": "Beat Hades with Calisthenics Program maxed"
     },
     {
-        "name": "Hit a Double bomber or 4 witch meg"
+        "name": "Hit both Bombers without middle management, all 4 Witches, or Power Couple in a single Meg"
     },
     {
-        "name": "Finish an Extreme Measures 4 run"
+        "name": "Beat Hades with Extreme Measures maxed"
     },
     {
-        "name": "Win with 3 different weapons"
+        "name": "Beat Hades without having used any keepsakes that force you to get certain gods"
     },
     {
-        "name": "32 heat clear"
+        "name": "Beat Hades with Convenience Fee maxed"
     },
     {
-        "name": "Underworld Customs clear"
+        "name": "Beat Hades with Underworld Customs turned on"
     },
     {
-        "name": "Use only 1 keepsake for a whole run"
+        "name": "Beat Hades with the keepsake you went into Tartarus with still equipped"
     },
     {
         "name": "Catch a rare fish"
@@ -29,22 +29,22 @@ var bingoList = [
         "name": "Maxed jury summons run"
     },
     {
-        "name": "Take a chaos boon that summons more enemies and finish it."
+        "name": "Finish a chaos curse that raises the number of foes"
     },
     {
         "name": "Catch 5 fish"
     },
     {
-        "name": "Beat Charon"
+        "name": "Beat a boss without taking damage"
     },
     {
         "name": "Earn the reward from an erebus gates"
     },
     {
-        "name": "Sub 9:30 IGT"
+        "name": "Beat Hades with Damage Control maxed"
     },
     {
-        "name": "Deal over 1000 damage in a single hit"
+        "name": "Deal over 1500 damage in a single instance from your weapon"
     },
     {
         "name": "Buy the meta reward in styx"
@@ -59,16 +59,16 @@ var bingoList = [
         "name": "Have a heroic boon"
     },
     {
-        "name": "Get boon to level 6+"
+        "name": "Get a boon to level +7"
     },
     {
-        "name": "Approval Process 2 Clear"
+        "name": "Beat Hades with Approval Process maxed"
     },
     {
         "name": "Clear 5 Styx Tunnels in one run you finish"
     },
     {
-        "name": "Clear a damage curse on your main means of attack from chaos without losing a DD"
+        "name": "Finish a damage chaos curse on your main damage source without losing a DD"
     },
     {
         "name": "Clear a biome with only dashes"
@@ -77,13 +77,13 @@ var bingoList = [
         "name": "Kill 4 enemies with a single trap activation"
     },
     {
-        "name": "Use 2 greater calls in a single non-boss room"
+        "name": "Beat Hades with Benefits package maxed"
     },
     {
         "name": "Finish a run with 6+ gods in your god pool"
     },
     {
-        "name": "Finish a boss fight with full hp"
+        "name": "Leave a boss chamber with full hp"
     },
     {
         "name": "Complete a trial of the gods without getting hit"
@@ -107,42 +107,90 @@ var bingoList = [
         "name": "Apply 4 status ailments to a single enemy"
     },
     {
-        "name": "Have 7 items with durations active from wells of charon at a single time"
+        "name": "Have 7 items in your boon list at once"
     },
     {
-        "name": "Take 4 attack focused hammers"
+        "name": "Take 3 attack focused hammers"
     },
     {
-        "name": "Take 4 special focused hammers"
+        "name": "Take 3 special focused hammers"
     },
     {
-        "name": "Obtain 5 different calls"
+        "name": "Obtain 4 different calls"
     },
     {
-        "name": "Obtain 5 different attack slot boons"
+        "name": "Obtain 4 different attack slot boons"
     },
     {
-        "name": "Obtain 5 different special slot boons"
+        "name": "Obtain 4 different special slot boons"
     },
     {
-        "name": "Obtain 5 different cast slot boons"
+        "name": "Obtain 4 different cast slot boons"
     },
     {
-        "name": "Obtain 5 different dash slot boons"
+        "name": "Obtain 4 different dash slot boons"
     },
     {
-        "name": "Get bad influence"
+        "name": "Greet the Shade with a Bouldy boon"
     },
     {
-        "name": "Gift bouldy nectar"
+        "name": "Beat Hades with 5 summons available"
     },
     {
-        "name": "Win a game with 5 summons availialbe"
+        "name": "Beat lernie with 3 different aspects"
     },
     {
-        "name": "Beat lernie with 4 different aspects"
+        "name": "Beat Hades with Middle Management maxed"
     },
     {
-        "name": "Get bad influence"
+        "name": "Get High Voltage"
+    },
+    {
+        "name": "Get Bad influence"
+    },
+    {
+        "name": "Get Black Metal"
+    },
+    {
+        "name": "Get Brilliant Riposte"
+    },
+    {
+        "name": "Get Hide Breaker"
+    },
+    {
+        "name": "Get Typhoons Fury"
+    },
+    {
+        "name": "Get Arctic Blast"
+    },
+    {
+        "name": "Beat Hades with the Chaos Egg Equipped"
+    },
+    {
+        "name": "Let Thanatos win one of his challenges"
+    },
+    {
+        "name": "Get out of Tartarus with Routine inspection maxed"
+    },
+    {
+        "name": "Leave Tartarus without having benefitted from a single boon once"
+    },
+    {
+        "name": "Beat Hades using all red mirror options"
+    },
+    {
+        "name": "Beat Hades with all green mirror options"
+    },
+    {
+        "name": "Deal over 2000 damage in a single attack/special"
+    },
+    {
+        "name": "Beat Hades with the aspect of the week"
+    },
+    {
+        "name": "Beat Hades with over 350 max hp"
+    },
+    {
+        "name": "Purge 3 boons at a single Pool of Purging"
     }
 ];
