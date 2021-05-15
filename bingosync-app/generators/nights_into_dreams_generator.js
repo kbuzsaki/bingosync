871ce2e4d7bd7f42bd06c84c97fea0187df35d12
Kyle Buzsaki <kbuzsaki@gmail.com>
2021-05-14 18:18:52 -0700

    Update NiGHTS generator

--- bingosync-app/generators/nights_into_dreams_generator.js
+++ bingosync-app/generators/nights_into_dreams_generator.js
@@ -1,52 +1,214 @@
-var bingoGenerator = require("./generators/generator_bases/simple_generator.js");
+bingoGenerator = require("./generators/generator_bases/simple_generator.js");
 
-var bingoList = [{"name": "Paraloop Gillwing in 1 hit"},
-{"name": "Paraloop/Drill 5 Nightmaren in a single level"},
-{"name": "Breed a Mepian"},
-{"name": "A Rank Spring Valley"},
-{"name": "A Rank Splash Garden"},
-{"name": "A Rank Mystic Forest"},
-{"name": "A Rank Frozen Bell"},
-{"name": "A Rank Soft Museum"},
-{"name": "A Rank Stick Canyon"},
-{"name": "A Rank Twin Seeds"},
-{"name": "Get a DREAMY using the trick ring"},
-{"name": "Breed 8 Nightopians"},
-{"name": "Defeat Reala with a score multiplier of 1.5% or lower"},
-{"name": "Get Link 25+ in Spring Valley Mare 1"},
-{"name": "Pop every stationary balloon on Twin Seeds Mare 4"},
-{"name": "Guide the Car to the Garage in Mystic Forest"},
-{"name": "Escape the tomb in Mystic Forest through the tree trunk"},
-{"name": "Grab 99+ Blue Chips in Splash Garden Mare 4"},
-{"name": "Complete Mare 1 of Stick Canyon as Elliot"},
-{"name": "Collect 75+ Blue Chips in Soft Museum as Claris"},
-{"name": "Grab the point multiplier in Frozen Bell"},
-{"name": "Collect 40+ Blue Chips in Frozen Bell as Elliot"},
-{"name": "Defeat Gulpo with a score multiplier of 2.0%"},
-{"name": "Defeat Puffy with a score multiplier of 1.9% or higher"},
-{"name": "Defeat Clawz without paralooping the fireworks"},
-{"name": "Complete any level with an F"},
-{"name": "Defeat Jackle with no drill"},
-{"name": "Defeat Wizeman with a score multiplier of 1.5% or lower"},
-{"name": "Destroy the ideya capture as Elliot in Stick Canyon Mare 4"},
-{"name": "Preform 15 triple jumps in a row"},
-{"name": "Find the 6 Star Chips under the waterfall in Splash Garden as Claris"},
-{"name": "Get 25+ Link in Mystic Forest"},
-{"name": "Collect the 9 Blue Chips behind the Mare 2 Ideya Capture in Soft Museum"},
-{"name": "Get Link 25+ in the autoscroller in Soft Museum"},
-{"name": "Become Elliot in the water section in Splash Garden"},
-{"name": "Defeat Gillwing but you can only press drill"},
- {"name": "Complete Mystic Forest Mare 3 without touching any rings"},
- {"name": "Complete Spring Vally Mare 2 without touching any rings"},
- {"name": "Perform 7 tricks while fighting and defeat Jackle"},
- {"name": "Perform 7 tricks while fighting and defeat Clawz"},
- {"name": "Complete Twin Seeds Mare 1 without touching any rings"},
- {"name": "Complete Stick Canyon Mare 4 going left as NiGHTS"},
- {"name": "Get 40+ Link in Twin Seeds"},
- {"name": "Collect 50+ Blue Chips in Splash Garden as Elliot without jumping"},
- {"name": "Collect 99+ Blue Chips in Splash Garden as Elliot"},
- {"name": "Collect 35,000 points in Spring Valley Mare 1"},
- {"name": "Collect 35,000 points in Mystic Forest Mare 1 "},
- {"name": "Complete Twin Seeds Mare 4 with exactly 20 Blue Chips"},
- {"name": "Defeat Wizeman without using drill dash"},
- {"name": "Defeat Gulpo without getting hit"}];
+var bingoList = [
+    {
+        "name": "Paraloop Gillwing in 1 hit"
+    },
+    {
+        "name": "Paraloop/Drill 5 Nightmaren in a single level"
+    },
+    {
+        "name": "Breed a Mepian"
+    },
+    {
+        "name": "A Rank Spring Valley"
+    },
+    {
+        "name": "A Rank Splash Garden"
+    },
+    {
+        "name": "A Rank Mystic Forest"
+    },
+    {
+        "name": "A Rank Frozen Bell"
+    },
+    {
+        "name": "A Rank Soft Museum"
+    },
+    {
+        "name": "A Rank Stick Canyon"
+    },
+    {
+        "name": "A Rank Twin Seeds"
+    },
+    {
+        "name": "Get a DREAMY using the trick ring"
+    },
+    {
+        "name": "Hatch 8 Nightopian eggs"
+    },
+    {
+        "name": "Defeat Reala with a score multiplier of 1.5% or lower"
+    },
+    {
+        "name": "Get Link 25+ in Spring Valley Mare 1"
+    },
+    {
+        "name": "Pop every stationary balloon on Twin Seeds Mare 4"
+    },
+    {
+        "name": "Guide the Car to the Garage in Mystic Forest"
+    },
+    {
+        "name": "Escape the tomb in Mystic Forest through the tree trunk"
+    },
+    {
+        "name": "Grab 99+ Blue Chips in Splash Garden Mare 4"
+    },
+    {
+        "name": "Complete Mare 1 of Stick Canyon as Elliot"
+    },
+    {
+        "name": "Collect 75+ Blue Chips in Soft Museum as Claris"
+    },
+    {
+        "name": "Grab the point multiplier in Frozen Bell"
+    },
+    {
+        "name": "Collect 40+ Blue Chips in Frozen Bell as Elliot"
+    },
+    {
+        "name": "Defeat Gulpo with a score multiplier of 2.0%"
+    },
+    {
+        "name": "Defeat Puffy with a score multiplier of 1.9% or higher"
+    },
+    {
+        "name": "Defeat Clawz without paralooping the fireworks"
+    },
+    {
+        "name": "Complete any level with an F"
+    },
+    {
+        "name": "Defeat Jackle without using drill"
+    },
+    {
+        "name": "Defeat Wizeman with a score multiplier of 1.5% or lower"
+    },
+    {
+        "name": "Destroy the ideya capture as Elliot in Stick Canyon Mare 4"
+    },
+    {
+        "name": "Preform 15 triple jumps in a row"
+    },
+    {
+        "name": "Find the 6 Star Chips under the waterfall in Spring Valley as Claris"
+    },
+    {
+        "name": "Get 25+ Link in Mystic Forest"
+    },
+    {
+        "name": "Collect the 9 Blue Chips behind the Mare 2 Ideya Capture in Soft Museum"
+    },
+    {
+        "name": "Get Link 25+ in the autoscroller in Soft Museum"
+    },
+    {
+        "name": "Become Elliot in the water section in Splash Garden"
+    },
+    {
+        "name": "Defeat Gillwing but you can only press drill"
+    },
+    {
+        "name": "Complete Mystic Forest Mare 3 without touching any rings"
+    },
+    {
+        "name": "Complete Spring Vally Mare 2 without touching any rings"
+    },
+    {
+        "name": "Perform 7 tricks while fighting and defeat Jackle"
+    },
+    {
+        "name": "Perform 7 tricks while fighting and defeat Clawz"
+    },
+    {
+        "name": "Complete Twin Seeds Mare 1 without touching any rings"
+    },
+    {
+        "name": "Complete Stick Canyon Mare 4 going left from the start as NiGHTS"
+    },
+    {
+        "name": "Get 40+ Link in Twin Seeds"
+    },
+    {
+        "name": "Collect 50+ Blue Chips in Splash Garden as Elliot without jumping"
+    },
+    {
+        "name": "Collect 99+ Blue Chips in Splash Garden as Elliot"
+    },
+    {
+        "name": "Collect 35,000 points in Spring Valley Mare 1"
+    },
+    {
+        "name": "Collect 35,000 points in Mystic Forest Mare 1"
+    },
+    {
+        "name": "Complete Twin Seeds Mare 4 with exactly 20 Blue Chips"
+    },
+    {
+        "name": "Defeat Wizeman without using drill dash"
+    },
+    {
+        "name": "Defeat Gulpo without getting hit"
+    },
+    {
+        "name": "Get 25+ Link in Frozen Bell Mare 1"
+    },
+    {
+        "name": "Complete Splash Garden Mare 1 without touching any rings"
+    },
+    {
+        "name": "Get 15+ Link in Splash Garden Mare 3"
+    },
+    {
+        "name": "Get 40+ Link in Stick Canyon Mare 1"
+    },
+    {
+        "name": "Complete Stick Canyon Mare 3 without activating the magnitizer"
+    },
+    {
+        "name": "Complete Spring Valley Mare 3 without touching any rings"
+    },
+    {
+        "name": "Get 30+ Link in Soft Museum Mare 2"
+    },
+    {
+        "name": "Avoid getting hit by a firework that is flying versus Clawz"
+    },
+    {
+        "name": "Complete Soft Museum Mare 2 without collecting any star chips"
+    },
+    {
+        "name": "Complete Soft Museum Mare 3 without using the autoscroller"
+    },
+    {
+        "name": "Complete Soft Museum Mare 4 without touching any rings"
+    },
+    {
+        "name": "Collect 50,000 points in Frozen Bell Mare 1"
+    },
+    {
+        "name": "Collect 200 Star Chips in Frozen Bell"
+    },
+    {
+        "name": "Collect 200 Star Chips in Spring Valley"
+    },
+    {
+        "name": "Complete Stick Canyon Mare 2 while only grabbing blue chips from the autoscroller"
+    },
+    {
+        "name": "Complete Stick Canyon Mare 4 without touching any rings"
+    },
+    {
+        "name": "Complete Soft Museum Mare 3 while only grabbing blue chips from the autoscroller"
+    },
+    {
+        "name": "Get launched as Claris in Spring Valley by 3 hills"
+    },
+    {
+        "name": "Hit the alarm clock as either Claris or Elliot out of bounds (purple goo)"
+    },
+    {
+        "name": "Collect 3 boost refills in Stick Canyon"
+    }
+];
