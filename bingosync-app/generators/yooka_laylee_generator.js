a710b8f3f1d37acba35d1fd6cff5af4cc808e3eb
Kyle Buzsaki <kbuzsaki@gmail.com>
2021-06-10 23:18:54 -0700

    Update Yooka-Laylee generator

--- bingosync-app/generators/yooka_laylee_generator.js
+++ bingosync-app/generators/yooka_laylee_generator.js
@@ -1,153 +1,153 @@
-bingoGenerator = require("./generators/generator_bases/srl_generator_v5.js");
+var bingoGenerator = require("./generators/generator_bases/srl_generator_v5.js");
 
 var bingoList = [];
 bingoList[1] = [
-  { name: "Learn all Tribalstack Moves", types: ["trowzer","tribal"] },
-  { name: "5 Hub Pagies", types:["hub","pagies"] },
-  { name: "all Shipwreck Quills", types:["collectible", "hub"] },
-  { name: "Tribal Planker - Flightless", types: ["pagie", "tribal", "challenge"] },
+  { name: "6 Sub-Area Pagies", types:["subarea", "pagie"] },
+  { name: "2 Health Extenders", types: ["extender"] },
+  { name: "All Shipwreck Quills", types:["quill", "hub"] },
+  { name: "Shipwreck Creek Pagies", types: ["hub", "pagie"] },
 ];
 bingoList[2] = [
-  { name: "Shipwreck Creek Pagies", types: ["hub", "pagies"] },
-  { name: "2 Energy Extenders", types: ["collectible"] },
-  { name: "Glitter Kartos", types: ["kartos", "glitter"] },
-  { name: "Learn 8 Trowzer moves", types: ["trowzer"] },
+  { name: "3 Extenders", types: ["extender"] },
+  { name: "2 Energy Extenders", types: ["extender"] },
+  { name: "10 Hub Pagies", types:["hub","pagie"] },
+  { name: "7 Tribal Pagies", types:["tribal","pagie"] },
 ];
 bingoList[3] = [
   { name: "Defeat The Great Rampo", types: ["boss", "tribal"] },
-  { name: "3 Extenders", types: ["collectible"] },
-  { name: "2 Pirate Treasures", types: ["collectible"] },
+  { name: "Tribal Bubble Pagie Intended", types:["tribal","pagie"] },
+  { name: "2 Pirate Treasures", types: ["pirate"] },
   { name: "Save 5 Ghost Writers", types: ["ghost"] },
 ];
 bingoList[4] = [
-  { name: "2 Health Extenders", types: ["collectible"] },
-  { name: "Expand 2 Tomes", types: ["tome"] },
-  { name: "Rampo w/out Flight", types:["boss", "tribal", "challenge"] },
-  { name: "10 Hub Pagies", types:["hub","pagies"] },
+  { name: "Help Planker 2 Times", types: ["pagie"] },
+  { name: "2 Playcoins", types: ["rextro"] },
+  { name: "Blasto & Blastooie", types: ["hub", "tribal", "pagie"] },
+  { name: "Glitter Kartos", types: ["kartos", "glitter"] },
 ];
 bingoList[5] = [
-  { name: "Kartos Karting", types: ["rextro", "tribal"] },
-  { name: "Help Planker 2 Times", types: ["pagie"] },
-  { name: "Blasto & Blastooie", types: ["hub", "tribal", "pagie"] },
-  { name: "Pagie Rampagie", types:["challenge", "pagie"] },
+  { name: "Help Clara 2 Times", types: ["pagie"] },
+  { name: "Tribal Ghost Writers", types: ["ghost", "tribal"] },
+  { name: "Rampo w/out Flight", types:["boss", "tribal", "challenge"] },
+  { name: "Tribal Kartos", types: ["kartos", "tribal"] },
 ];
 bingoList[6] = [
-  { name: "Unlock Capital Cashino", types: ["tome"] },
-  { name: "Freezing Room - Intended Way", types: ["pagie", "glitter", "challenge"] },
-  { name: "Expand Glitter Glacier", types: ["tome"] },
+  { name: "Icy Nimble Race", types: ["tribal", "pagie"] },
+  { name: "Quiz 2", types:["quiz"] },
+  { name: "4 Extenders", types: ["extender"] },
   { name: "Toxic Pipe Pagie, No Sonar Sheild/Reptile Rush", types: ["pagie", "hub", "challenge"] },
 ];
 bingoList[7] = [
-  { name: "Purchase Lizard Leap First", types: ["trowzer", "glitter", "challenge"] },
+  { name: "Quiz 1", types:["quiz"] },
   { name: "3 Green Ghost Writers", types: ["ghost"] },
-  { name: "4 Extenders", types: ["collectible"] },
-  { name: "Tribal Kartos", types: ["kartos", "tribal"] },
-];
-bingoList[8] = [
-  { name: "3 Yellow Ghost Writers", types: ["ghost"] },
-  { name: "200 Quills", types: ["collectible"] },
-  { name: "Help Clara 2 Times", types: ["pagie"] },
-  { name: "Quiz 2", types:["quiz", "challenge"] },
-];
-bingoList[9] = [
-  { name: "Race Nimble 2 Times", types: ["tribal", "pagie"] },
-  { name: "Tribal Ghost Writers", types: ["ghost", "tribal"] },
-  { name: "Defeat Brrreeze Blok", types: ["boss", "glitter", "subarea"] },
-  { name: "Cashino Kartos", types: ["kartos", "cashino"] },
-];
-bingoList[10] = [
-  { name: "Learn all Glitter Moves", types: ["trowzer", "glitter"] },
-  { name: "Unlock Moody Marsh", types: ["tome"] },
-  { name: "Glaciators", types: ["rextro", "glitter"] },
-  { name: "3 Pirate Treasures", types: ["collectible"] },
-];
-bingoList[11] = [
-  { name: "Quiz 1", types:["quiz", "challenge"] },
-  { name: "3 Blue Ghost Writers", types: ["ghost"] },
-  { name: "Brrreeze intended way", types:["boss", "glitter", "challenge", "subarea"] },
-  { name: "2 Pagies from Unique Transformation", types: ["pagie", "transformation"] },
-];
-bingoList[12] = [
-  { name: "Defeat 2 Bosses", types:["boss"] },
-  { name: "Collect 2 Pagies from the Knights of Hamalot", types: ["pagie"] },
-  { name: "Windtunnel Pagie, No Reptile Rush", types: ["pagie", "hub", "challenge"] },
+  { name: "200 Quills", types: ["quill"] },
   { name: "Learn 10 Trowzer moves", types: ["trowzer"] },
 ];
-bingoList[13] = [
-  { name: "5 Extenders", types: ["collectible"] },
+bingoList[8] = [
+  { name: "Kartos Karting Highscore", types: ["rextro", "tribal"] },
+  { name: "2 Pagies from Unique Transformation", types: ["pagie", "transformation"] },
+  { name: "Windtunnel Pagie, No Reptile Rush", types: ["pagie", "hub", "challenge"] },
+  { name: "Freezing Room - Intended Way", types: ["pagie", "glitter", "challenge"] },
+];
+bingoList[9] = [
+  { name: "3 Yellow Ghost Writers", types: ["ghost"] },
+  { name: "Equip your first tonic in Cashino", types:["challenge"] },
+  { name: "Cashino Kartos", types: ["kartos", "cashino"] },
+  { name: "50 Quills in 3 Worlds", types: ["quill"] },
+];
+bingoList[10] = [
+  { name: "Glaciators", types: ["rextro", "glitter", "subarea"] },
+  { name: "2 Kartos Pagies", types: ["kartos"] },
+  { name: "5 Extenders", types: ["extender"] },
+  { name: "3 Pirate Treasures", types: ["pirate"] },
+];
+bingoList[11] = [
+  { name: "Bee Bop", types: ["rextro", "moody"] },
+  { name: "3 Blue Ghost Writers", types: ["ghost"] },
   { name: "30 Tokens from broken slot machines", types: ["pagie", "cashino", "transformation"] },
+  { name: "Purchase Lizard Leap First", types: ["trowzer", "glitter", "challenge"] },
+];
+bingoList[12] = [
+  { name: "Thorns Intended Way", types: ["transformation", "moody", "pagie"] },
+  { name: "Collect 2 Pagies from the Knights of Hamalot", types: ["pagie"] },
+  { name: "10 Glitter Pagies", types:["glitter","pagie"] },
+  { name: "Gun-tlet Highscore", types: ["rextro"] },
+];
+bingoList[13] = [
+  { name: "Defeat Brrreeze Blok", types: ["boss", "glitter", "subarea"] },
   { name: "10 Sub-Area Pagies", types:["subarea", "pagie"] },
-  { name: "50 Quills in 3 Worlds", types: ["collectible"] },
+  { name: "Blag Flag Highscore", types: ["rextro"] },
+  { name: "Jobstacle Highscore", types: ["rextro"] },
 ];
 bingoList[14] = [
-  { name: "2 Kartos Pagies", types: ["kartos"] },
-  { name: "Moody Kartos", types: ["kartos", "moody"] },
-  { name: "Bee Bop", types: ["rextro", "moody"] },
+  { name: "Learn all Glitter Moves", types: ["trowzer", "glitter"] },
+  { name: "8 Moody Pagies", types:["moody","pagie"] },
+  { name: "8 Cashino Pagies", types:["cashino","pagie"] },
   { name: "6 Transformation Pagies", types: ["pagie", "transformation"] },
 ];
 bingoList[15] = [
   { name: "3 Red Ghost Writers", types: ["ghost"] },
   { name: "Save 10 Ghost Writers", types: ["ghost"] },
   { name: "15 Hub Pagies", types:["hub","pagies"] },
-  { name: "Equip your first tonic in Cashino", types:["challenge"] },
+  { name: "Defeat 2 Bosses", types:["boss"] },
 ];
 bingoList[16] = [
-  { name: "Learn all Moody Moves", types: ["trowzer", "moody"] },
+  { name: "Moody Kartos", types: ["kartos", "moody"] },
   { name: "Help 2 Trolleys", types: ["pagie", "moody"] },
-  { name: "2 Rextro Pagies", types: ["rextro"] },
-  { name: "Defeat Trev the Tenteyecle", types: ["boss", "moody"] },
+  { name: "4 Pirate Treasures", types: ["pirate"] },
+  { name: "3 Kartos Pagies", types: ["kartos"] },
 ];
 bingoList[17] = [
   { name: "2 Pink Ghost Writers", types: ["ghost"] },
-  { name: "Expand 3 Tomes", types: ["tome"] },
-  { name: "100 Quills in 2 Worlds", types: [ "collectible"] },
-  { name: "Defeat I.N.E.P.T.", types: ["boss", "cashino", "kartos"] },
+  { name: "4 Playcoins", types: ["rextro"] },
+  { name: "3 Pagies from Unique Transformation", types: ["pagie", "transformation"] },
+  { name: "Brrreeze Intended Way", types:["boss", "glitter", "challenge", "subarea"] },
 ];
 bingoList[18] = [
-  { name: "Hurdle Hijinx", types: ["kartos", "cashino"] },
+  { name: "Collect All Hub Pagies", types: ["pagie", "hub"] },
   { name: "Glitter Ghost Writers", types: ["ghost", "glitter"] },
-  { name: "4 Pirate Treasures", types: ["collectible"] },
-  { name: "3 Pagies from Unique Transformation", types: ["pagie", "transformation"] },
+  { name: "Learn 12 Trowzer moves", types: ["trowzer"] },
+  { name: "100 Quills in 2 Worlds", types: [ "quill"] },
 ];
 bingoList[19] = [
-  { name: "Defeat 3 Bosses", types:["boss"] },
-  { name: "Learn 12 Trowzer moves", types: ["trowzer"] },
-  { name: "3 Kartos Pagies", types: ["kartos"] },
-  { name: "150 Quills in 1 World", types: ["collectible"] },
+  { name: "Learn all Moody Moves", types: ["trowzer", "moody"] },
+  { name: "30 Quills in 4 Worlds", types: ["quill"] },
+  { name: "8 Extenders", types: ["extender"] },
+  { name: "Defeat Trev the Tenteyecle", types: ["boss", "moody"] },
 ];
 bingoList[20] = [
-  { name: "30 Quills in 4 Worlds", types: ["collectible"] },
-  { name: "Expand Moody Marsh", types: ["tome"] },
+  { name: "14 Sub-Area Pagies", types:["subarea", "pagie"] },
+  { name: "Defeat I.N.E.P.T.", types: ["boss", "cashino", "kartos"] },
   { name: "300 Quills", types: ["collectible"] },
-  { name: "Collect All Hub Pagies", types: ["completionist", "pagie", "hub"] },
+  { name: "Moodymaze Health Extender", types:["extender", "moody"] },
 ];
 bingoList[21] = [
-  { name: "Expand Capital Cashino", types: ["tome"] },
-  { name: "Open Moody First", types:["tome", "challenge"] },
-  { name: "Moodymaze Health Extender", types:["challenge", "collectible", "moody"] },
-  { name: "15 Sub-Area Pagies", types:["subarea", "pagie"] },
+  { name: "All Mollycools", types: ["galleon", "transformation"] },
+  { name: "Save 15 Ghost Writers", types: ["ghost"] },
+  { name: "Moody Ghost Writers", types: ["ghost", "moody"] },
+  { name: "Hurdle Hijinx", types: ["rextro", "cashino"] },
 ];
 bingoList[22] = [
-  { name: "Moody Ghost Writers", types: ["ghost", "moody"] },
-  { name: "Unlock Galleon Galaxy", types: ["tome"] },
-  { name: "4 Rextro Pagies", types: ["rextro"] },
-  { name: "Defeat Cap B. w/out Flight", types:["boss", "challenge"] },
+  { name: "15 Quills in 5 Worlds", types: ["quill", "galleon"] },
+  { name: "Cashino Ghost Writers", types: ["ghost", "cashino"] },
+  { name: "Pirate Ship Pagie", types: ["galleon", "transformation"] },
+  { name: "3 Galleon Ghosts", types: ["ghost", "galleon"] },
 ];
 bingoList[23] = [
-  { name: "Defeat 4 Bosses", types:["boss"] },
-  { name: "Cashino Ghost Writers", types: ["ghost", "cashino"] },
-  { name: "Unlock All Grand Tomes", types: ["tome", "completionist"] },
+  { name: "Galleon Energy Extender", types: ["extender", "galleon"] },
+  { name: "4 Pink Ghost Writers", types: ["ghost"] },
+  { name: "Defeat 3 Bosses", types:["boss"] },
   { name: "Complete 2 Pachinko Machines", types:["challenge", "cashino"] },
 ];
 bingoList[24] = [
   { name: "Quiz 3 then 2 then 1", types:["quiz", "challenge"] },
-  { name: "Save 15 Ghost Writers", types: ["ghost"] },
-  { name: "15 Quills in 5 Worlds", types: ["collectible"] },
-  { name: "Beat Capital B with Butterthree", types:["boss", "challenge"] },
+  { name: "150 Quills in 1 World", types: ["quill"] },
+  { name: "4 Pagies from Unique Transformation", types: ["pagie", "transformation"] },
+  { name: "100 Tokens at Once", types:["challenge", "cashino", "pagie"] },
 ];
 bingoList[25] = [
   { name: "400 Quills", types: ["collectible"] },
-  { name: "All Mollycools", types: ["completionist", "collectible"] },
-  { name: "100 Tokens at Once", types:["challenge", "cashino", "pagie"] },
-  { name: "20 Sub-Area Pagies", types:["subarea", "pagie"] },
+  { name: "18 Sub-Area Pagies", types:["subarea", "pagie"] },
+  { name: "Defeat Capital B", types:["boss", "quiz"] },
+  { name: "Open Moody First", types:["tome", "challenge", "moody"] },
 ];
