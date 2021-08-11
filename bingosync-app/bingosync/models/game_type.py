8289092ba0cd500634f4975c0b01ce45074d4570
Kyle Buzsaki <kbuzsaki@gmail.com>
2021-08-10 22:29:28 -0700

    Add KHFM Rando

--- bingosync-app/bingosync/models/game_type.py
+++ bingosync-app/bingosync/models/game_type.py
@@ -300,6 +300,7 @@ class GameType(Enum):
     pokemon_crystal_randomizer_tournament = 296
     new_super_mario_bros_2 = 297
     crash_bandicoot_4 = 298
+    kingdom_hearts_final_mix_randomizer = 299
 
     def __str__(self):
         return self.short_name
@@ -608,6 +609,13 @@ GAME_GROUPS = {
             (GameType.jak_and_daxter_new_game_plus, "NG+", "J&D: TPL NG+"),
         ],
     },
+    GameType.kingdom_hearts_final_mix: {
+        "name": "Kingdom Hearts Final Mix",
+        "variants": [
+            (GameType.kingdom_hearts_final_mix, "Normal", "KHFM"),
+            (GameType.kingdom_hearts_final_mix_randomizer, "Randomizer", "KHFM Rando"),
+        ],
+    },
     GameType.kingdom_hearts_2_final_mix: {
         "name": "Kingdom Hearts II Final Mix",
         "variants": [
@@ -909,7 +917,6 @@ GAME_GROUPS = {
     **singleton_group(GameType.iconoclasts, "Iconoclasts", "Iconoclasts"),
     **singleton_group(GameType.into_the_breach, "Into the Breach", "ITB"),
     **singleton_group(GameType.jade_cocoon, "Jade Cocoon: Story of the Tamamayu", "Jade Cocoon: SotT"),
-    **singleton_group(GameType.kingdom_hearts_final_mix, "Kingdom Hearts Final Mix", "KHFM"),
     **singleton_group(GameType.kirby_and_the_amazing_mirror, "Kirby & The Amazing Mirror", "KtAM"),
     **singleton_group(GameType.kotor, "Star Wars: Knights of the Old Republic", "KotOR"),
     **singleton_group(GameType.league_of_legends_aram, "League of Legends ARAM", "LoL ARAM"),
