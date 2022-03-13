d5edb96c85d59b99168bfe1c6cc3a92e601c1258
TJ <68974572+tht1kd@users.noreply.github.com>
2022-03-12 22:55:14 -0600

    Update horizon_zero_dawn_generator.js

--- bingosync-app/generators/horizon_zero_dawn_generator.js
+++ bingosync-app/generators/horizon_zero_dawn_generator.js
@@ -1,40 +1,110 @@
-bingoGenerator = require("./generators/generator_bases/simple_generator.js");
+bingoGenerator = require("./generators/generator_bases/srl_generator_v5.js");
 
-var bingoList = [
-    "Collect 5 Metal Flowers",
-    "Collect 10 Metal Flowers",
-    "Collect 3 Banuk Figures",
-    "Collect 3 Vantage Points",
-    "Collect 5 Vantage Points",
-    "Get 10 Bluegleam",
-    "Get 20 Bluegleam",
-    "Collect 2 Ancient Vessels",
-    "Collect 6 Ancient Vessels",
-    "Clear 3 Corruption Zones",
-    "Clear 6 Corruption Zones",
-    "Clear a Bandit Camp",
-    "Clear 3 Bandit Camps",
-    "Clear SIGMA Cauldron",
-    "Clear RHO Cauldron",
-    "Clear XI Cauldron",
-    "Clear ZETA Cauldron",
-    "Override 1 Tallneck",
-    "Override 3 Tallnecks",
-    "Collect 3 Pigments",
-    "Collect 6 Pigments",
-    "Don't Fast Travel",
-    "Don't Override Machines",
-    "Kill 4 Bellowbacks",
-    "Kill a Rockbreaker",
-    "Override 2 Stormbirds",
-    "Kill 2 Stormbirds",
-    "Kill 6 snapmaws",
-    "Override 2 Thunderjaws",
-    "Kill 2 Thunderjaws",
-    "Collect 2 Powercells",
-	"Collect All Rising Suns from a Hunting Ground",
-	"Finish 'In Her Mother's Footsteps' Sidequest",
-    "Finish 'Acquired Taste' Sidequest",
-    "Finish 'Fatal Inheritance' Sidequest",
-	"Finish 'Insult to Injury' Sidequest"
+var bingoList = [];
+bingoList[1] = [
+	{ name: "Collect 5 Metal Flowers"},
+	{ name: "Finish 'In Her Mother's Footsteps' Sidequest"},
+	{ name: "Destroy 10 Grazer Dummies"}
+];
+bingoList[2] = [
+	{ name: "Clear a Bandit Camp"},
+	{ name: "Collect 4 Ancient Vessels"},
+	{ name: "Collect 3 Vantage Points"}
+];
+bingoList[3] = [
+	{ name: "Collect 10 Metal Flowers"},
+	{ name: "Collect 2 Banuk Figures"},
+	{ name: "Collect 6 Ancient Vessels"}
+];
+bingoList[4] = [
+	{ name: "Don't Fast Travel"},
+	{ name: "Collect 15 Metal Flowers"},
+	{ name: "Kill 4 Red-Eyed Watchers"}
+];
+bingoList[5] = [
+	{ name: "Collect 4 Banuk Figures"},
+	{ name: "Override 2 Tallnecks"},
+	{ name: "Collect 6 Vantage Points"},
+	{ name: "Collect a Full Sun from 4 different Hunting Grounds"}
+];
+bingoList[6] = [
+	{ name: "Clear 3 Bandit Camp"},
+	{ name: "Collect All Blazing Suns from a Hunting Ground"},
+	{ name: "Finish 'Insult to Injury' Sidequest"}
+];
+bingoList[7] = [
+	{ name: "Harvest 10 Blaze Containers from Grazers"},
+	{ name: "Raid 5 Intact Shell Walker Containers"}
+];
+bingoList[8] = [
+	{ name: "Clear 3 Corruption Zones"},
+	{ name: "Clear SIGMA Cauldron"},
+	{ name: "Collect a Blazing Sun from 3 different Hunting Grounds"},
+	{ name: "Kill 8 Bellowbacks"}
+];
+bingoList[9] = [
+	{ name: "Clear Blackwing Snag Bandit Camp"},
+	{ name: "Kill 6 Snapmaws"},
+	{ name: "Obtain 4 Purple Modifications"}
+];
+bingoList[10] = [
+	{ name: "Clear Shattered Kiln Bandit Camp"},
+	{ name: "Complete 6 Errands"}
+];
+bingoList[11] = [
+	{ name: "Clear 7 Corruption Zones"},
+	{ name: "Override 4 Tallnecks"}
+];
+bingoList[12] = [
+	{ name: "Collect 3 Pigments"},
+	{ name: "Collect 15 Bluegleam"}
+];
+bingoList[13] = [
+	{ name: "Collect 6 Pigments"},
+	{ name: "Destroy 4 Control Towers"},
+	{ name: "Complete 8 Errands"}
+];
+bingoList[14] = [
+	{ name: "Collect 25 Bluegleam"},
+	{ name: "Finish 'Fatal Inheritance' Sidequest"}
+];
+bingoList[15] = [
+	{ name: "Defeat the Stormbird at Gaia Prime"},
+	{ name: "Clear RHO Cauldron"},
+	{ name: "Complete 12 Errands"}
+];
+bingoList[16] = [
+	{ name: "Kill a Rockbreaker"},
+	{ name: "Clear XI Cauldron"}
+];
+bingoList[17] = [
+	{ name: "Clear ZETA Cauldron"}
+];
+bingoList[18] = [
+	{ name: "Kill 3 Thunderjaws"},
+	{ name: "Kill 2 Stormbirds"}
+];
+bingoList[19] = [
+	{ name: "Use a Ropecaster to tie-down 3 Stormbirds"}
+];
+bingoList[20] = [
+	{ name: "Override 4 Types of Machines"}
+];
+bingoList[21] = [
+	{ name: "Override 8 Types of Machines"},
+	{ name: "Collect All Blazing Suns from 5 Hunting Grounds"}
+];
+bingoList[22] = [
+	{ name: "Kill 2 Frostclaws"}
+];
+bingoList[23] = [
+	{ name: "Override 3 Thunderjaws"},
+	{ name: "Override 4 Stormbirds"}
+];
+bingoList[24] = [
+	{ name: "Finish 'Acquired Taste' Sidequest"}
+];
+bingoList[25] = [
+	{ name: "Get First Place in 'Ikrie's Challenge'"},
+	{ name: "Collect 3 Power Cells"}
 ];
