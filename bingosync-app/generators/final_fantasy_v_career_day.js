525212e05f77dd2a025d707c356231c20d03e272
alraedris <132615309+alraedris@users.noreply.github.com>
2023-05-04 15:50:15 -0500

    ffv-career-day.js
    Creating a list of jobs

---
+++ bingosync-app/generators/final_fantasy_v_career_day.js
@@ -0,0 +1,150 @@
+bingoGenerator = require("./generators/generator_bases/srl_generator_v5.js");
+
+var bingoList = [];
+bingoList[1] = [
+    { name: "Bingo Check", types: ["category"] },
+  ];
+
+bingoList[2] = [
+];
+
+bingoList[3] = [
+];
+
+bingoList[4] = [
+];
+
+bingoList[5] = [
+];
+
+bingoList[6] = [
+];
+
+bingoList[7] = [
+];
+
+bingoList[8] = [
+];
+
+bingoList[9] = [
+];
+
+bingoList[10] = [
+];
+
+bingoList[11] = [
+];
+
+bingoList[12] = [
+];
+
+bingoList[13] = [
+];
+
+bingoList[14] = [
+];
+
+bingoList[15] = [
+];
+
+bingoList[16] = [
+];
+
+bingoList[17] = [
+];
+
+bingoList[18] = [
+];
+
+bingoList[19] = [
+];
+
+bingoList[20] = [
+];
+
+bingoList[21] = [
+];
+
+bingoList[22] = [
+];
+
+bingoList[23] = [
+];
+
+bingoList[24] = [
+    { name: "Collect all 17 non-required Key Items", types: ["completion"]},
+];
+
+bingoList[25] = [
+    { name: "Collect All Jobs", types: ["completion"]},
+];
+
+
+
+
+
+
+
+
+Groupings
+["legendary"] - Collection of the 12 tablet items (not checks, weapons)
+["equipment"] - Find any of the listed equiment items
+["jobs"] - Find certain jobs
+["jobs-crystal"] - Find jobs within the crystals themselves
+["key-items"] - Key item Collection
+["bosses"] - Defeat certain bosses
+["bosses-ex"] - Unique boss groupings
+["hint"] - Use the hint system
+["location"] - Clear a location 
+["greg"] - Greg. Need I say more?
+UNASSIGNED
+{ name: "Get the Brave Blade", types: ["equipment"] },
+{ name: "Get the Chicken Blade", types: ["equipment"] },
+{ name: "Get Ragnarok", types: ["equipment"] },
+{ name: "Get Excalibur", types: ["equipment"] },
+{ name: "Get Tinkerbell", types: ["equipment"] },
+{ name: "Get Artemis Bow", types: ["equipment"] },
+{ name: "Get Thor's Hammer", types: ["equipment"] },
+{ name: "Get the Apollo Harp", types: ["equipment"] },
+{ name: "Get the Masamune", types: ["equipment"] },
+{ name: "Get the Dragon Lance", types: ["equipment"] },
+{ name: "Get the Judgement Staff", types: ["equipment"] },
+{ name: "Get the Dragon Whisker", types: ["equipment"] },
+{ name: "Get 3 legendary weapons", types: ["legendary"] },
+{ name: "Get 6 legendary weapons", types: ["legendary"] },
+{ name: "Get 9 legendary weapons", types: ["legendary"] },
+{ name: "Get 3 of the Wind Jobs (KNT|MON|THF|BLM|WHM|BLU)", types: ["jobs-crystal"]},
+{ name: "Get 3 of the Water Jobs (RED|TIM|SUM|BRS|MYS)", types: ["jobs-crystal"]},
+{ name: "Get 3 of the Fire Jobs (BST|GEO|NIN|HUN|BRD)", types: ["jobs-crystal"] },
+{ name: "Get 2 of the Earth Jobs (DRG|DNC|SAM|CHM)", types: ["jobs-crystal"] },
+{ name: "Get the Freelancer crystal", types: ["jobs"] },
+{ name: "Get the Mime crystal", types: ["jobs"] },
+{ name: "Get any 3 key items (Excluding Anti-barrier, Bracelet, Adamantite)", types: ["key-items"] },
+{ name: "Get any 5 key items (Excluding Anti-barrier, Bracelet, Adamantite)", types: ["key-items"] },
+{ name: "Get any 8 key items (Excluding Anti-barrier, Bracelet, Adamantite)", types: ["key-items"] },
+{ name: "Get any 11 key items (Excluding Anti-barrier, Bracelet, Adamantite)", types: ["key-items"] },
+{ name: "Defeat any 5 summon bosses (Shiva|Ifrit|Ramuh|Shoat|Carbuncle|Golem|Odin|Bahamut|Leviathan|Titan)", types: ["bosses-ex"] },
+{ name: "Defeat any 3 of the Void bosses (Calofisteri|Apocalypse|Apanda|Catastrophe|Halicarnassus|Twin Tania|Necrofobia)", types: ["bosses-ex"] },
+{ name: "Defeat any 2 of the Ship bosses (Karlabos|Siren|Liquid Flame|Gilga-3)", types: ["bosses-ex"] },
+{ name: "Defeat any 2 of the Mountain bosses (Magisa|Hiryuu Plant|Bahamut)", types: ["bosses-ex"] },
+{ name: "Defeat any 2 of the Tower bosses (Galura|Atomos|Minotauros|Omniscient)", types: ["bosses-ex"] },
+{ name: "Defeat any 3 of the Castle bosses (Shiva|Abductor|Gilga-1|Carbuncle|Gilga-4)", types: ["bosses-ex"] },
+{ name: "Defeat all of the Library bosses (Ifrit|Byblos)", types: ["bosses-ex"] },
+{ name: "Defeat any 3 of the Meteor bosses (AdamanTimi|Chimera|Puroboros|Titan)", types: ["bosses-ex"] },
+{ name: "Defeat any 2 of the Aerial bosses (Cray Claw|Sol Cannon|Archaevoaevis)", types: ["bosses-ex"] },
+{ name: "Defeat all of the Desert bosses (Sandworm|Merugene)", types: ["bosses-ex"] },
+{ name: "Defeat all of the Burning bosses (Iron Claw|Guardian)", types: ["bosses-ex"] },
+{ name: "Defeat any 2 of the Cave bosses (Tyrosaur|Antlion|Odin)", types: ["bosses-ex"] },
+{ name: "Defeat all of the Bridge bosses (Gilga-2|Stalker)", types: ["bosses-ex"] },
+{ name: "Defeat all of the Water bosses (Triplets|Leviathan|Gogo)", types: ["bosses-ex"] },
+{ name: "Kill Sekmet", types: ["bosses-ex"] },
+{ name: "Check all W1 hints (Tycoon|Tule|Karnak|Walse|Library)", "types": ["hint"] },
+{ name: "Check all W2 hints (Surgate|Kelb|Bal|Moore|Rugor)", "types": ["hint"] },
+{ name: "Clear Fork Tower", types: ["location"] },
+{ name: "Clear Shrine Page", types: ["location"] },
+{ name: "Clear Pyramid Page", types: ["location"] },
+{ name: "Clear Trench Page", types: ["location"] },
+{ name: "Clear Falls Page", types: ["location"] },
+{ name: "Mua Forest", types: ["location"] },
+{ name: "See at least 3 Gilgamesh encounters", types: ["greg"] },
+{ name: "Kill Apocalypse with Self-Destruct", types: ["???"] },
