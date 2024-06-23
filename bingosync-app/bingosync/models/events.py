b23d3b4e3192e7034573f3e8efc906aa4a83b0b8
Kyle Buzsaki <kbuzsaki@gmail.com>
2024-06-23 08:56:35 -0700

    Use select_related to prefetch relevant event fields and avoid N+1 queries.

--- bingosync-app/bingosync/models/events.py
+++ bingosync-app/bingosync/models/events.py
@@ -20,6 +20,14 @@ class Event(models.Model):
     def json_timestamp(self):
         return self.timestamp.replace().timestamp()
 
+    @classmethod
+    def default_qs(cls, room, **kwargs):
+        """ Per-class overridable hook for building a queryset that pre-loads relevant fields.
+
+            This avoids the N+1 query problem when rendering the events to json.
+        """
+        return cls.objects.filter(player__room=room).select_related("player")
+
     @staticmethod
     def event_classes():
         return [ChatEvent, GoalEvent, ColorEvent, RevealedEvent, ConnectionEvent, NewCardEvent]
@@ -28,16 +36,18 @@ class Event(models.Model):
     def get_all_for_room(room):
         all_events = []
         for event_class in Event.event_classes():
-            all_events.extend(event_class.objects.filter(player__room=room))
+            all_events.extend(event_class.default_qs(room=room))
         return sorted(all_events, key=lambda event: event.timestamp)
 
     @staticmethod
     def get_all_recent_for_room(room):
+        cutoff_dt = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(hours=24)
         recent_events = []
         total_events = 0;
         for event_class in Event.event_classes():
             total_events += event_class.objects.filter(player__room=room).count()
-            recent_events.extend(event_class.objects.filter(player__room=room).filter(timestamp__gte=datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(hours=24)))
+            # TODO: is it worth trying to index on the timestamp field?
+            recent_events.extend(event_class.default_qs(room=room).filter(timestamp__gte=cutoff_dt))
         all_included = total_events == len(recent_events)
         recent_events = sorted(recent_events, key=lambda event: event.timestamp)
         return {'events': recent_events, 'all_included': all_included}
@@ -78,14 +88,27 @@ class NewCardEvent(Event):
     seed = models.IntegerField(default=0)
     hide_card = models.BooleanField(default=False)
 
+    @classmethod
+    def default_qs(cls, room, **kwargs):
+        """ Override includes the timestamp of the latest card for this room to fast-path is_current."""
+        room_new_cards = NewCardEvent.objects.filter(player__room=room).values('player__room')
+        latest_ts = room_new_cards.annotate(latest_ts=models.Max("timestamp")).values('latest_ts')
+        return super(NewCardEvent, cls).default_qs(room, **kwargs).annotate(_latest_timestamp=models.Subquery(latest_ts))
+
     @property
     def game_type(self):
         return GameType.for_value(self.game_type_value)
 
     @property
     def is_current(self):
-        new_card_events = NewCardEvent.objects.filter(player__room=self.player.room).order_by("timestamp")
-        return new_card_events.last() == self
+        # TODO: technically the newcard event with the latest timestamp is not necessarily the current game
+        # However this has been the behavior since the feature was implemented.
+        if hasattr(self, "_latest_timestamp"):
+            latest_timestamp = self._latest_timestamp
+        else:
+            new_card_events = NewCardEvent.objects.filter(player__room=self.player.room).order_by("timestamp")
+            latest_timestamp = new_card_events.last().timestamp
+        return latest_timestamp == self.timestamp
 
     def to_json(self):
         return {
@@ -103,6 +126,11 @@ class GoalEvent(Event):
     square = models.ForeignKey("bingosync.Square", on_delete=models.CASCADE)
     color_value = models.IntegerField(choices=Color.goal_choices())
     remove_color = models.BooleanField(default=False)
+
+    @classmethod
+    def default_qs(cls, **kwargs):
+        """ Override includes the square because it will immediately be included in the json. """
+        return super(GoalEvent, cls).default_qs(**kwargs).select_related("square")
 
     @property
     def color(self):
